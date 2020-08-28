7703add66664954cca6ac6d042ee93deca70bee6
Francesco Biscani <bluescarni@gmail.com>
2020-08-28 20:07:04 +0200

    Finish wiring up the exp() Taylor support.

--- src/math_functions.cpp
+++ src/math_functions.cpp
@@ -594,7 +594,7 @@ llvm::Value *taylor_diff_batch_log_impl(llvm_state &s, std::uint32_t idx, const 
                                         std::uint32_t batch_size, std::uint32_t vector_size,
                                         const std::unordered_map<std::uint32_t, number> &)
 {
-    // NOTE: also not much use in allowing zero-order
+    // NOTE: not much use in allowing zero-order
     // derivatives, which in general might complicate
     // the implementation.
     if (order == 0u) {
@@ -800,6 +800,132 @@ expression log(expression e)
     return expression{std::move(fc)};
 }
 
+namespace detail
+{
+
+namespace
+{
+
+template <typename T>
+llvm::Value *taylor_init_batch_exp(llvm_state &s, const function &f, llvm::Value *arr, std::uint32_t batch_idx,
+                                   std::uint32_t batch_size, std::uint32_t vector_size)
+{
+    if (f.args().size() != 1u) {
+        throw std::invalid_argument("Inconsistent number of arguments in the Taylor initialization phase for "
+                                    "the exponential (1 argument was expected, but "
+                                    + std::to_string(f.args().size()) + " arguments were provided");
+    }
+
+    return taylor_init_batch_unary_func<T>(s, f, arr, batch_idx, batch_size, vector_size);
+}
+
+// Derivative of exp(number).
+template <typename T>
+llvm::Value *taylor_diff_batch_exp_impl(llvm_state &s, std::uint32_t, const number &, std::uint32_t, std::uint32_t,
+                                        llvm::Value *, std::uint32_t, std::uint32_t, std::uint32_t vector_size,
+                                        const std::unordered_map<std::uint32_t, number> &)
+{
+    return taylor_diff_batch_zero<T>(s, vector_size);
+}
+
+// Derivative of exp(variable).
+template <typename T>
+llvm::Value *taylor_diff_batch_exp_impl(llvm_state &s, std::uint32_t idx, const variable &var, std::uint32_t order,
+                                        std::uint32_t n_uvars, llvm::Value *diff_arr, std::uint32_t batch_idx,
+                                        std::uint32_t batch_size, std::uint32_t vector_size,
+                                        const std::unordered_map<std::uint32_t, number> &)
+{
+    // NOTE: pairwise summation requires order 1 at least.
+    // NOTE: not much use in allowing zero-order
+    // derivatives, which in general might complicate
+    // the implementation.
+    if (order == 0u) {
+        throw std::invalid_argument(
+            "Cannot compute the Taylor derivative of order 0 of exp() (the order must be at least one)");
+    }
+
+    auto &builder = s.builder();
+
+    // Fetch the index of the variable.
+    const auto u_idx = uname_to_index(var.name());
+
+    // NOTE: iteration in the [0, order) range
+    // (i.e., order excluded).
+    std::vector<llvm::Value *> sum;
+    for (std::uint32_t j = 0; j < order; ++j) {
+        // The indices for accessing the derivatives in this loop iteration:
+        // - j * n_uvars * batch_size + idx * batch_size + batch_idx,
+        // - (order - j) * n_uvars * batch_size + u_idx * batch_size + batch_idx.
+        auto arr_ptr0 = builder.CreateInBoundsGEP(
+            diff_arr, {builder.getInt32(0), builder.getInt32(j * n_uvars * batch_size + idx * batch_size + batch_idx)},
+            "exp_ptr");
+        auto arr_ptr1
+            = builder.CreateInBoundsGEP(diff_arr,
+                                        {builder.getInt32(0), builder.getInt32((order - j) * n_uvars * batch_size
+                                                                               + u_idx * batch_size + batch_idx)},
+                                        "exp_ptr");
+
+        // Load the values.
+        auto v0 = (vector_size == 0u) ? builder.CreateLoad(arr_ptr0, "exp_load")
+                                      : load_vector_from_memory(builder, arr_ptr0, vector_size, "exp_load");
+        auto v1 = (vector_size == 0u) ? builder.CreateLoad(arr_ptr1, "exp_load")
+                                      : load_vector_from_memory(builder, arr_ptr1, vector_size, "exp_load");
+
+        auto fac = codegen<T>(s, number(static_cast<T>(order - j)));
+        if (vector_size > 0u) {
+            fac = create_constant_vector(builder, fac, vector_size);
+        }
+
+        // Add (order-j)*v0*v1 to the sum.
+        sum.push_back(builder.CreateFMul(fac, builder.CreateFMul(v0, v1)));
+    }
+
+    // Init the return value as the result of the sum.
+    auto ret_acc = llvm_pairwise_sum(builder, sum);
+
+    // Finalise the return value: ret_acc / n.
+    auto div = codegen<T>(s, number(static_cast<T>(order)));
+    if (vector_size > 0u) {
+        div = create_constant_vector(builder, div, vector_size);
+    }
+
+    return builder.CreateFDiv(ret_acc, div);
+}
+
+// All the other cases.
+template <typename T, typename U>
+llvm::Value *taylor_diff_batch_exp_impl(llvm_state &, std::uint32_t, const U &, std::uint32_t, std::uint32_t,
+                                        llvm::Value *, std::uint32_t, std::uint32_t, std::uint32_t,
+                                        const std::unordered_map<std::uint32_t, number> &)
+{
+    throw std::invalid_argument(
+        "An invalid argument type was encountered while trying to build the Taylor derivative of an exponential");
+}
+
+template <typename T>
+llvm::Value *taylor_diff_batch_exp(llvm_state &s, const function &func, std::uint32_t idx, std::uint32_t order,
+                                   std::uint32_t n_uvars, llvm::Value *diff_arr, std::uint32_t batch_idx,
+                                   std::uint32_t batch_size, std::uint32_t vector_size,
+                                   const std::unordered_map<std::uint32_t, number> &cd_uvars)
+{
+    if (func.args().size() != 1u) {
+        throw std::invalid_argument("Inconsistent number of arguments in the Taylor derivative for "
+                                    "the exponential (1 argument was expected, but "
+                                    + std::to_string(func.args().size()) + " arguments were provided");
+    }
+
+    return std::visit(
+        [&](const auto &v) {
+            return taylor_diff_batch_exp_impl<T>(s, idx, v, order, n_uvars, diff_arr, batch_idx, batch_size,
+                                                 vector_size, cd_uvars);
+        },
+        func.args()[0].value());
+}
+
+} // namespace
+
+} // namespace detail
+
 expression exp(expression e)
 {
     std::vector<expression> args;
@@ -873,16 +999,16 @@ expression exp(expression e)
 
         return std::exp(args[0]);
     };
-//    fc.taylor_init_batch_dbl_f() = detail::taylor_init_batch_log<double>;
-//    fc.taylor_init_batch_ldbl_f() = detail::taylor_init_batch_log<long double>;
-//#if defined(HEYOKA_HAVE_REAL128)
-//    fc.taylor_init_batch_f128_f() = detail::taylor_init_batch_log<mppp::real128>;
-//#endif
-//    fc.taylor_diff_batch_dbl_f() = detail::taylor_diff_batch_log<double>;
-//    fc.taylor_diff_batch_ldbl_f() = detail::taylor_diff_batch_log<long double>;
-//#if defined(HEYOKA_HAVE_REAL128)
-//    fc.taylor_diff_batch_f128_f() = detail::taylor_diff_batch_log<mppp::real128>;
-//#endif
+    fc.taylor_init_batch_dbl_f() = detail::taylor_init_batch_exp<double>;
+    fc.taylor_init_batch_ldbl_f() = detail::taylor_init_batch_exp<long double>;
+#if defined(HEYOKA_HAVE_REAL128)
+    fc.taylor_init_batch_f128_f() = detail::taylor_init_batch_exp<mppp::real128>;
+#endif
+    fc.taylor_diff_batch_dbl_f() = detail::taylor_diff_batch_exp<double>;
+    fc.taylor_diff_batch_ldbl_f() = detail::taylor_diff_batch_exp<long double>;
+#if defined(HEYOKA_HAVE_REAL128)
+    fc.taylor_diff_batch_f128_f() = detail::taylor_diff_batch_exp<mppp::real128>;
+#endif
 
     return expression{std::move(fc)};
 }
---
+++ test/taylor_exp.cpp
@@ -0,0 +1,404 @@
+// Copyright 2020 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <heyoka/config.hpp>
+
+#include <cmath>
+#include <initializer_list>
+#include <random>
+#include <tuple>
+#include <vector>
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+#include <mp++/real128.hpp>
+
+#endif
+
+#include <heyoka/expression.hpp>
+#include <heyoka/llvm_state.hpp>
+#include <heyoka/math_functions.hpp>
+#include <heyoka/number.hpp>
+
+#include "catch.hpp"
+#include "test_utils.hpp"
+
+static std::mt19937 rng;
+
+using namespace heyoka;
+using namespace heyoka_test;
+
+const auto fp_types = std::tuple<double, long double
+#if defined(HEYOKA_HAVE_REAL128)
+                                 ,
+                                 mppp::real128
+#endif
+                                 >{};
+
+template <typename T, typename U>
+void compare_batch_scalar(std::initializer_list<U> sys, unsigned opt_level)
+{
+    const auto batch_size = 23u;
+
+    llvm_state s{kw::opt_level = opt_level};
+
+    s.add_taylor_jet_batch<T>("jet_batch", sys, 3, batch_size);
+    s.add_taylor_jet_batch<T>("jet_scalar", sys, 3, 1);
+
+    s.compile();
+
+    auto jptr_batch = s.fetch_taylor_jet_batch<T>("jet_batch");
+    auto jptr_scalar = s.fetch_taylor_jet_batch<T>("jet_scalar");
+
+    std::vector<T> jet_batch;
+    jet_batch.resize(8 * batch_size);
+    std::uniform_real_distribution<float> dist(.1f, 20.f);
+    std::generate(jet_batch.begin(), jet_batch.end(), [&dist]() { return T{dist(rng)}; });
+
+    std::vector<T> jet_scalar;
+    jet_scalar.resize(8);
+
+    jptr_batch(jet_batch.data());
+
+    for (auto batch_idx = 0u; batch_idx < batch_size; ++batch_idx) {
+        // Assign the initial values of x and y.
+        for (auto i = 0u; i < 2u; ++i) {
+            jet_scalar[i] = jet_batch[i * batch_size + batch_idx];
+        }
+
+        jptr_scalar(jet_scalar.data());
+
+        for (auto i = 2u; i < 8u; ++i) {
+            REQUIRE(jet_scalar[i] == approximately(jet_batch[i * batch_size + batch_idx]));
+        }
+    }
+}
+
+TEST_CASE("taylor exp")
+{
+    auto tester = [](auto fp_x, unsigned opt_level) {
+        using std::exp;
+
+        using fp_t = decltype(fp_x);
+
+        using Catch::Matchers::Message;
+
+        auto x = "x"_var, y = "y"_var;
+
+        // Number tests.
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(expression{number{fp_t(2)}}), x + y}, 1, 1);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{3}};
+            jet.resize(4);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == 3);
+            REQUIRE(jet[2] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[3] == 5);
+        }
+
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(expression{number{fp_t(2)}}), x + y}, 1, 2);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{-2}, fp_t{3}, fp_t{-3}};
+            jet.resize(8);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == -2);
+
+            REQUIRE(jet[2] == 3);
+            REQUIRE(jet[3] == -3);
+
+            REQUIRE(jet[4] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[5] == approximately(exp(fp_t{2})));
+
+            REQUIRE(jet[6] == 5);
+            REQUIRE(jet[7] == -5);
+        }
+
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(expression{number{fp_t(2)}}), x + y}, 2, 1);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{3}};
+            jet.resize(6);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == 3);
+            REQUIRE(jet[2] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[3] == 5);
+            REQUIRE(jet[4] == 0);
+            REQUIRE(jet[5] == approximately(fp_t{1} / 2 * (jet[3] + exp(fp_t{2}))));
+        }
+
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(expression{number{fp_t(2)}}), x + y}, 2, 2);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{-2}, fp_t{3}, fp_t{-3}};
+            jet.resize(12);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == -2);
+
+            REQUIRE(jet[2] == 3);
+            REQUIRE(jet[3] == -3);
+
+            REQUIRE(jet[4] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[5] == approximately(exp(fp_t{2})));
+
+            REQUIRE(jet[6] == 5);
+            REQUIRE(jet[7] == -5);
+
+            REQUIRE(jet[8] == 0);
+            REQUIRE(jet[9] == 0);
+
+            REQUIRE(jet[10] == approximately(fp_t{1} / 2 * (jet[6] + exp(fp_t{2}))));
+            REQUIRE(jet[11] == approximately(fp_t{1} / 2 * (jet[7] + exp(fp_t{2}))));
+        }
+
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(expression{number{fp_t(2)}}), x + y}, 3, 3);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{-2}, fp_t{1}, fp_t{3}, fp_t{-3}, fp_t{0}};
+            jet.resize(24);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == -2);
+            REQUIRE(jet[2] == 1);
+
+            REQUIRE(jet[3] == 3);
+            REQUIRE(jet[4] == -3);
+            REQUIRE(jet[5] == 0);
+
+            REQUIRE(jet[6] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[7] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[8] == approximately(exp(fp_t{2})));
+
+            REQUIRE(jet[9] == 5);
+            REQUIRE(jet[10] == -5);
+            REQUIRE(jet[11] == 1);
+
+            REQUIRE(jet[12] == 0);
+            REQUIRE(jet[13] == 0);
+            REQUIRE(jet[14] == 0);
+
+            REQUIRE(jet[15] == approximately(fp_t{1} / 2 * (jet[9] + exp(fp_t{2}))));
+            REQUIRE(jet[16] == approximately(fp_t{1} / 2 * (jet[10] + exp(fp_t{2}))));
+            REQUIRE(jet[17] == approximately(fp_t{1} / 2 * (jet[11] + exp(fp_t{2}))));
+
+            REQUIRE(jet[18] == 0);
+            REQUIRE(jet[19] == 0);
+            REQUIRE(jet[20] == 0);
+
+            REQUIRE(jet[21] == approximately(fp_t{1} / 6 * (2 * jet[15])));
+            REQUIRE(jet[22] == approximately(fp_t{1} / 6 * (2 * jet[16])));
+            REQUIRE(jet[23] == approximately(fp_t{1} / 6 * (2 * jet[17])));
+        }
+
+        // Do the batch/scalar comparison.
+        compare_batch_scalar<fp_t>({exp(expression{number{fp_t(2)}}), x + y}, opt_level);
+
+        // Variable tests.
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(y), exp(x)}, 1, 1);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{3}};
+            jet.resize(4);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == 3);
+            REQUIRE(jet[2] == approximately(exp(fp_t{3})));
+            REQUIRE(jet[3] == approximately(exp(fp_t{2})));
+        }
+
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(y), exp(x)}, 1, 2);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{4}, fp_t{3}, fp_t{5}};
+            jet.resize(8);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == 4);
+
+            REQUIRE(jet[2] == 3);
+            REQUIRE(jet[3] == 5);
+
+            REQUIRE(jet[4] == approximately(exp(fp_t{3})));
+            REQUIRE(jet[5] == approximately(exp(fp_t{5})));
+
+            REQUIRE(jet[6] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[7] == approximately(exp(fp_t{4})));
+        }
+
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(y), exp(x)}, 2, 1);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{3}};
+            jet.resize(6);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == 3);
+            REQUIRE(jet[2] == approximately(exp(fp_t{3})));
+            REQUIRE(jet[3] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[4] == approximately(fp_t{1} / 2 * jet[2] * jet[3]));
+            REQUIRE(jet[5] == approximately(fp_t{1} / 2 * jet[2] * jet[3]));
+        }
+
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(y), exp(x)}, 2, 2);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{4}, fp_t{3}, fp_t{5}};
+            jet.resize(12);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == 4);
+
+            REQUIRE(jet[2] == 3);
+            REQUIRE(jet[3] == 5);
+
+            REQUIRE(jet[4] == approximately(exp(fp_t{3})));
+            REQUIRE(jet[5] == approximately(exp(fp_t{5})));
+
+            REQUIRE(jet[6] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[7] == approximately(exp(fp_t{4})));
+
+            REQUIRE(jet[8] == approximately(fp_t{1} / 2 * jet[4] * jet[6]));
+            REQUIRE(jet[9] == approximately(fp_t{1} / 2 * jet[5] * jet[7]));
+
+            REQUIRE(jet[10] == approximately(fp_t{1} / 2 * jet[4] * jet[6]));
+            REQUIRE(jet[11] == approximately(fp_t{1} / 2 * jet[5] * jet[7]));
+        }
+
+        {
+            llvm_state s{kw::opt_level = opt_level};
+
+            s.add_taylor_jet_batch<fp_t>("jet", {exp(y), exp(x)}, 3, 3);
+
+            s.compile();
+
+            auto jptr = s.fetch_taylor_jet_batch<fp_t>("jet");
+
+            std::vector<fp_t> jet{fp_t{2}, fp_t{4}, fp_t{3}, fp_t{3}, fp_t{5}, fp_t{6}};
+            jet.resize(24);
+
+            jptr(jet.data());
+
+            REQUIRE(jet[0] == 2);
+            REQUIRE(jet[1] == 4);
+            REQUIRE(jet[2] == 3);
+
+            REQUIRE(jet[3] == 3);
+            REQUIRE(jet[4] == 5);
+            REQUIRE(jet[5] == 6);
+
+            REQUIRE(jet[6] == approximately(exp(fp_t{3})));
+            REQUIRE(jet[7] == approximately(exp(fp_t{5})));
+            REQUIRE(jet[8] == approximately(exp(fp_t{6})));
+
+            REQUIRE(jet[9] == approximately(exp(fp_t{2})));
+            REQUIRE(jet[10] == approximately(exp(fp_t{4})));
+            REQUIRE(jet[11] == approximately(exp(fp_t{3})));
+
+            REQUIRE(jet[12] == approximately(fp_t{1} / 2 * jet[6] * jet[9]));
+            REQUIRE(jet[13] == approximately(fp_t{1} / 2 * jet[7] * jet[10]));
+            REQUIRE(jet[14] == approximately(fp_t{1} / 2 * jet[8] * jet[11]));
+
+            REQUIRE(jet[15] == approximately(fp_t{1} / 2 * jet[6] * jet[9]));
+            REQUIRE(jet[16] == approximately(fp_t{1} / 2 * jet[7] * jet[10]));
+            REQUIRE(jet[17] == approximately(fp_t{1} / 2 * jet[8] * jet[11]));
+
+            REQUIRE(jet[18] == approximately(fp_t{1} / 6 * (jet[6] * jet[9] * jet[9] + jet[6] * 2 * jet[12])));
+            REQUIRE(jet[19] == approximately(fp_t{1} / 6 * (jet[7] * jet[10] * jet[10] + jet[7] * 2 * jet[13])));
+            REQUIRE(jet[20] == approximately(fp_t{1} / 6 * (jet[8] * jet[11] * jet[11] + jet[8] * 2 * jet[14])));
+
+            REQUIRE(jet[21] == approximately(fp_t{1} / 6 * (jet[9] * jet[6] * jet[6] + jet[9] * 2 * jet[15])));
+            REQUIRE(jet[22] == approximately(fp_t{1} / 6 * (jet[10] * jet[7] * jet[7] + jet[10] * 2 * jet[16])));
+            REQUIRE(jet[23] == approximately(fp_t{1} / 6 * (jet[11] * jet[8] * jet[8] + jet[11] * 2 * jet[17])));
+        }
+
+        // Do the batch/scalar comparison.
+        compare_batch_scalar<fp_t>({exp(y), exp(x)}, opt_level);
+    };
+
+    tuple_for_each(fp_types, [&tester](auto x) { tester(x, 0); });
+    tuple_for_each(fp_types, [&tester](auto x) { tester(x, 1); });
+    tuple_for_each(fp_types, [&tester](auto x) { tester(x, 2); });
+    tuple_for_each(fp_types, [&tester](auto x) { tester(x, 3); });
+}
