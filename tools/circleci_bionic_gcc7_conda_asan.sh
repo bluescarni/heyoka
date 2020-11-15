77a937eda25d48fe2537039b41edcd26e5f4abd0
Francesco Biscani <bluescarni@gmail.com>
2020-11-15 16:53:26 +0100

    First stab at circleci.

---
+++ tools/circleci_bionic_gcc7_conda_asan.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install build-essential wget
+
+# Install conda+deps.
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda_pkgs="cmake llvmdev boost-cpp mp++ sleef"
+conda create -q -p $deps_dir -y
+source activate $deps_dir
+conda install mamba -y
+mamba install $conda_pkgs -y
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# GCC build.
+cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_WITH_MPPP=yes -DHEYOKA_WITH_SLEEF=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" -DHEYOKA_ENABLE_IPO=yes
+make -j2 VERBOSE=1
+ctest -V -j2
+
+set +e
+set +x
