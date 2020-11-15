bfd34b4861b933a8cb17ff16307545bef964db94
Francesco Biscani <bluescarni@gmail.com>
2020-11-15 23:38:30 +0100

    Don't try to use ninja.

--- tools/circleci_bionic_gcc7_conda_asan.sh
+++ tools/circleci_bionic_gcc7_conda_asan.sh
@@ -16,7 +16,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge
 conda config --set channel_priority strict
-conda_pkgs="cmake llvmdev boost-cpp mppp sleef xtensor xtensor-blas blas blas-devel ninja"
+conda_pkgs="cmake llvmdev boost-cpp mppp sleef xtensor xtensor-blas blas blas-devel"
 conda create -q -p $deps_dir -y
 source activate $deps_dir
 conda install mamba -y
@@ -27,9 +27,9 @@ mkdir build
 cd build
 
 # GCC build.
-cmake -GNinja ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_WITH_MPPP=yes -DHEYOKA_WITH_SLEEF=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" -DHEYOKA_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON
-ninja
-ctest -VV -j2
+cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_WITH_MPPP=yes -DHEYOKA_WITH_SLEEF=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" -DHEYOKA_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON
+make -j2 VERBOSE=1
+ctest -V -j2
 
 set +e
 set +x
--- tools/gha_osx.sh
+++ tools/gha_osx.sh
@@ -13,7 +13,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge
 conda config --set channel_priority strict
-conda_pkgs="c-compiler cxx-compiler libcxx cmake llvmdev boost-cpp sleef xtensor xtensor-blas blas blas-devel ninja"
+conda_pkgs="c-compiler cxx-compiler libcxx cmake llvmdev boost-cpp sleef xtensor xtensor-blas blas blas-devel"
 conda create -q -p $deps_dir -y
 source activate $deps_dir
 conda install mamba -y
@@ -24,9 +24,9 @@ mkdir build
 cd build
 
 # GCC build.
-CXX=clang++ CC=clang cmake -GNinja ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_WITH_SLEEF=yes -DHEYOKA_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON
-ninja
-ctest -VV -j2
+CXX=clang++ CC=clang cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_WITH_SLEEF=yes -DHEYOKA_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON
+make -j2 VERBOSE=1
+ctest -V -j2
 
 set +e
 set +x
