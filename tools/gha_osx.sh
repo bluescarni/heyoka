ffaedcabc6636dab20c53d7008dade0080c9c753
Francesco Biscani <bluescarni@gmail.com>
2020-11-15 23:10:47 +0100

    More CI tweaks.

--- tools/gha_osx.sh
+++ tools/gha_osx.sh
@@ -13,7 +13,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge
 conda config --set channel_priority strict
-conda_pkgs="clang clangxx libcxx cmake llvmdev boost-cpp sleef xtensor xtensor-blas"
+conda_pkgs="c-compiler cxx-compiler libcxx cmake llvmdev boost-cpp sleef xtensor xtensor-blas blas blas-devel"
 conda create -q -p $deps_dir -y
 source activate $deps_dir
 conda install mamba -y
