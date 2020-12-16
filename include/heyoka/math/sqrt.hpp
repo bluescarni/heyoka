d7b654edec9e48d04d9e8a29cb32cdeb42507505
Francesco Biscani <bluescarni@gmail.com>
2020-12-16 15:00:29 +0100

    Add headers for all remaining math functions.

---
+++ include/heyoka/math/sqrt.hpp
@@ -0,0 +1,50 @@
+// Copyright 2020 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef HEYOKA_MATH_SQRT_HPP
+#define HEYOKA_MATH_SQRT_HPP
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <heyoka/config.hpp>
+#include <heyoka/detail/fwd_decl.hpp>
+#include <heyoka/detail/llvm_fwd.hpp>
+#include <heyoka/detail/visibility.hpp>
+#include <heyoka/func.hpp>
+
+namespace heyoka
+{
+
+namespace detail
+{
+
+class HEYOKA_DLL_PUBLIC sqrt_impl : public func_base
+{
+public:
+    sqrt_impl();
+    explicit sqrt_impl(expression);
+
+    llvm::Value *codegen_dbl(llvm_state &, const std::vector<llvm::Value *> &) const;
+    llvm::Value *codegen_ldbl(llvm_state &, const std::vector<llvm::Value *> &) const;
+#if defined(HEYOKA_HAVE_REAL128)
+    llvm::Value *codegen_f128(llvm_state &, const std::vector<llvm::Value *> &) const;
+#endif
+
+    double eval_dbl(const std::unordered_map<std::string, double> &) const;
+    void eval_batch_dbl(std::vector<double> &, const std::unordered_map<std::string, std::vector<double>> &) const;
+    double eval_num_dbl(const std::vector<double> &) const;
+    double deval_num_dbl(const std::vector<double> &, std::vector<double>::size_type) const;
+};
+
+} // namespace detail
+
+} // namespace heyoka
+
+#endif
