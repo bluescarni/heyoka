cbccd47c0c59ac04937b6c7b954d2d18e57ddf83
Francesco Biscani <bluescarni@gmail.com>
2020-12-18 15:25:19 +0100

    First attempt at doc uploading.

--- tools/circleci_bionic_gcc7_conda_asan.sh
+++ tools/circleci_bionic_gcc7_conda_asan.sh
@@ -16,8 +16,9 @@ export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge
 conda config --set channel_priority strict
-conda create -y -q -p $deps_dir cmake llvmdev boost-cpp mppp sleef xtensor xtensor-blas blas blas-devel fmt
+conda create -y -q -p $deps_dir cmake llvmdev boost-cpp mppp sleef xtensor xtensor-blas blas blas-devel fmt sphinx pip
 source activate $deps_dir
+pip install --user sphinx-book-theme
 
 # Create the build dir and cd into it.
 mkdir build
@@ -27,6 +28,53 @@ cd build
 cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_BUILD_TUTORIALS=ON -DHEYOKA_WITH_MPPP=yes -DHEYOKA_WITH_SLEEF=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" -DHEYOKA_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON
 make -j2 VERBOSE=1
 ctest -V -j2
+
+# Build the docs.
+cd ../doc
+export SPHINX_OUTPUT=`make html linkcheck 2>&1`;
+if [[ "${SPHINX_OUTPUT}" != "" ]]; then
+    echo "Sphinx encountered some problem:";
+    echo "${SPHINX_OUTPUT}";
+    exit 1;
+fi
+echo "Sphinx ran successfully";
+
+if [[ ! -z "${CI_PULL_REQUEST}" ]]; then
+    echo "Testing a pull request, the generated documentation will not be uploaded.";
+    exit 0;
+fi
+
+if [[ "${CIRCLE_BRANCH}" != "master" ]]; then
+    echo "Branch is not master, the generated documentation will not be uploaded.";
+    exit 0;
+fi
+
+# Check out the gh_pages branch in a separate dir.
+cd ../
+git config --global push.default simple
+git config --global user.name "CircleCI"
+git config --global user.email "bluescarni@gmail.com"
+set +x
+git clone "https://${GH_TOKEN}@github.com/bluescarni/heyoka.git" heyoka_gh_pages -q
+set -x
+cd heyoka_gh_pages
+git checkout -b gh-pages --track origin/gh-pages;
+git rm -fr *;
+mv ../doc/_build/html/* .;
+git add *;
+# We assume here that a failure in commit means that there's nothing
+# to commit.
+git commit -m "Update Sphinx documentation, commit ${CIRCLE_SHA1} [skip ci]." || exit 0
+PUSH_COUNTER=0
+until git push -q
+do
+    git pull -q
+    PUSH_COUNTER=$((PUSH_COUNTER + 1))
+    if [ "$PUSH_COUNTER" -gt 3 ]; then
+        echo "Push failed, aborting.";
+        exit 1;
+    fi
+done
 
 set +e
 set +x
