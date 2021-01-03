1e8ac40f16d5322ca37d83a6c5ccd2b0e14f21e7
Francesco Biscani <bluescarni@gmail.com>
2021-01-03 14:04:54 +0100

    Remove unused includes.

--- include/heyoka/variable.hpp
+++ include/heyoka/variable.hpp
@@ -9,8 +9,6 @@
 #ifndef HEYOKA_VARIABLE_HPP
 #define HEYOKA_VARIABLE_HPP
 
-#include <heyoka/config.hpp>
-
 #include <cstddef>
 #include <cstdint>
 #include <ostream>
@@ -18,12 +16,6 @@
 #include <type_traits>
 #include <unordered_map>
 #include <vector>
-
-#if defined(HEYOKA_HAVE_REAL128)
-
-#include <mp++/real128.hpp>
-
-#endif
 
 #include <heyoka/detail/fwd_decl.hpp>
 #include <heyoka/detail/llvm_fwd.hpp>
