c38c85851c2cd77a89d0640c3132338d998b020d
Francesco Biscani <bluescarni@gmail.com>
2021-01-05 01:11:40 +0100

    Remove a header which is not really necessary.

--- src/detail/math_wrappers.cpp
+++ src/detail/math_wrappers.cpp
@@ -7,14 +7,13 @@
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <heyoka/config.hpp>
+#include <heyoka/detail/visibility.hpp>
 
 #if defined(HEYOKA_HAVE_REAL128)
 
 #include <mp++/real128.hpp>
 
 #endif
-
-#include <heyoka/detail/math_wrappers.hpp>
 
 #if defined(HEYOKA_HAVE_REAL128)
 
@@ -25,62 +24,68 @@
 static_assert(sizeof(__float128) == sizeof(mppp::real128));
 static_assert(alignof(__float128) == alignof(mppp::real128));
 
-__float128 heyoka_pow128(__float128 x, __float128 y)
+// NOTE: these wrappers are needed as a replacement
+// for the LLVM builtins, which seem to have issues
+// when invoked with __float128 arguments. This may be
+// related to incompatibilities between GCC
+// and LLVM in the implementation of __float128,
+// and needs to be investigated more.
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_pow128(__float128 x, __float128 y)
 {
     return mppp::pow(mppp::real128{x}, mppp::real128{y}).m_value;
 }
 
-__float128 heyoka_log128(__float128 x)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_log128(__float128 x)
 {
     return mppp::log(mppp::real128{x}).m_value;
 }
 
-__float128 heyoka_exp128(__float128 x)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_exp128(__float128 x)
 {
     return mppp::exp(mppp::real128{x}).m_value;
 }
 
-__float128 heyoka_sin128(__float128 x)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_sin128(__float128 x)
 {
     return mppp::sin(mppp::real128{x}).m_value;
 }
 
-__float128 heyoka_cos128(__float128 x)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_cos128(__float128 x)
 {
     return mppp::cos(mppp::real128{x}).m_value;
 }
 
-__float128 heyoka_sqrt128(__float128 x)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_sqrt128(__float128 x)
 {
     return mppp::sqrt(mppp::real128{x}).m_value;
 }
 
-__float128 heyoka_fma128(__float128 x, __float128 y, __float128 z)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_fma128(__float128 x, __float128 y, __float128 z)
 {
     return mppp::fma(mppp::real128{x}, mppp::real128{y}, mppp::real128{z}).m_value;
 }
 
-__float128 heyoka_maxabs128(__float128 x, __float128 y)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_maxabs128(__float128 x, __float128 y)
 {
     return mppp::fmax(mppp::real128{x}, mppp::abs(mppp::real128{y})).m_value;
 }
 
-__float128 heyoka_minabs128(__float128 x, __float128 y)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_minabs128(__float128 x, __float128 y)
 {
     return mppp::fmin(mppp::real128{x}, mppp::abs(mppp::real128{y})).m_value;
 }
 
-__float128 heyoka_minnum128(__float128 x, __float128 y)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_minnum128(__float128 x, __float128 y)
 {
     return mppp::fmin(mppp::real128{x}, mppp::real128{y}).m_value;
 }
 
-__float128 heyoka_abs128(__float128 x)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_abs128(__float128 x)
 {
     return mppp::abs(mppp::real128{x}).m_value;
 }
 
-__float128 heyoka_tan128(__float128 x)
+extern "C" HEYOKA_DLL_PUBLIC __float128 heyoka_tan128(__float128 x)
 {
     return mppp::tan(mppp::real128{x}).m_value;
 }
@@ -91,7 +96,10 @@ __float128 heyoka_tan128(__float128 x)
 
 #include <cmath>
 
-long double heyoka_tanl(long double x)
+// NOTE: there seems issues when trying to invoke the tanl()
+// function on MSVC (LLVM complaining about missing symbol).
+// Let's create an ad-hoc wrapper.
+extern "C" HEYOKA_DLL_PUBLIC long double heyoka_tanl(long double x)
 {
     return std::tan(x);
 }
