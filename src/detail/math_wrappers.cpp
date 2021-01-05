5fe88eab749be1a1b0d0c7509b7a05ab3f626312
Francesco Biscani <bluescarni@gmail.com>
2021-01-05 02:03:21 +0100

    Minor.

--- src/detail/math_wrappers.cpp
+++ src/detail/math_wrappers.cpp
@@ -20,7 +20,7 @@
 // Make sure that the size and alignment of __float128
 // and mppp::real128 coincide. This is required if we
 // want to be able to use mppp::real128 as an alias
-// for __float128 (e.g., in the llvm_state::fetch() bits).
+// for __float128.
 static_assert(sizeof(__float128) == sizeof(mppp::real128));
 static_assert(alignof(__float128) == alignof(mppp::real128));
 
