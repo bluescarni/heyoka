3ed3ca7246bd0556e0c59e6b89ac26a9512b56a5
Francesco Biscani <bluescarni@gmail.com>
2021-02-04 10:08:09 +0100

    Ensure that pow(x, 0) is always simplified to 1.

---
+++ test/pow.cpp
@@ -0,0 +1,60 @@
+// Copyright 2020, 2021 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <heyoka/config.hpp>
+
+// #include <functional>
+// #include <initializer_list>
+// #include <limits>
+
+// #include <boost/algorithm/string/predicate.hpp>
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+#include <mp++/real128.hpp>
+
+#endif
+
+#include <heyoka/expression.hpp>
+// #include <heyoka/llvm_state.hpp>
+// #include <heyoka/number.hpp>
+// #include <heyoka/taylor.hpp>
+#include <heyoka/math/pow.hpp>
+
+#include "catch.hpp"
+
+using namespace heyoka;
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+using namespace mppp::literals;
+
+#endif
+
+TEST_CASE("pow expo 1")
+{
+    auto x = "x"_var;
+
+    REQUIRE(heyoka::pow(x, 0.) == 1_dbl);
+    REQUIRE(heyoka::pow(x, 0.l) == 1_dbl);
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+    REQUIRE(heyoka::pow(x, 0._rq) == 1_dbl);
+
+#endif
+
+    REQUIRE(heyoka::pow(x, expression{0.}) == 1_dbl);
+    REQUIRE(heyoka::pow(x, expression{0.l}) == 1_dbl);
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+    REQUIRE(heyoka::pow(x, expression{0._rq}) == 1_dbl);
+
+#endif
+}
