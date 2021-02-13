b7477c938acf4c74053e17fb58292f5e0dfe096c
Francesco Biscani <bluescarni@gmail.com>
2021-02-13 22:32:24 +0100

    Additional testing.

--- test/taylor_tc.cpp
+++ test/taylor_tc.cpp
@@ -53,13 +53,37 @@ TEST_CASE("taylor tc basic")
 
                 auto tca = xt::adapt(ta.get_tc_data(), {2u, ta.get_order() + 1u});
 
-                auto [_, h] = ta.step(true);
+                auto [oc, h] = ta.step(true);
 
                 auto ret = xt::eval(xt::zeros<double>({2}));
 
                 horner_eval(ret, tca, static_cast<int>(ta.get_order()), 0);
                 REQUIRE(ret[0] == approximately(0.05, 10.));
                 REQUIRE(ret[1] == approximately(0.025, 10.));
+
+                horner_eval(ret, tca, static_cast<int>(ta.get_order()), h);
+                REQUIRE(ret[0] == approximately(ta.get_state()[0], 10.));
+                REQUIRE(ret[1] == approximately(ta.get_state()[1], 10.));
+
+                auto old_state = ta.get_state();
+
+                std::tie(oc, h) = ta.step_backward(true);
+
+                horner_eval(ret, tca, static_cast<int>(ta.get_order()), 0);
+                REQUIRE(ret[0] == approximately(old_state[0], 10.));
+                REQUIRE(ret[1] == approximately(old_state[1], 10.));
+
+                horner_eval(ret, tca, static_cast<int>(ta.get_order()), h);
+                REQUIRE(ret[0] == approximately(ta.get_state()[0], 10.));
+                REQUIRE(ret[1] == approximately(ta.get_state()[1], 10.));
+
+                old_state = ta.get_state();
+
+                std::tie(oc, h) = ta.step(1e-3, true);
+
+                horner_eval(ret, tca, static_cast<int>(ta.get_order()), 0);
+                REQUIRE(ret[0] == approximately(old_state[0], 10.));
+                REQUIRE(ret[1] == approximately(old_state[1], 10.));
 
                 horner_eval(ret, tca, static_cast<int>(ta.get_order()), h);
                 REQUIRE(ret[0] == approximately(ta.get_state()[0], 10.));
@@ -96,19 +120,67 @@ TEST_CASE("taylor tc basic")
                     auto sa = xt::adapt(ta.get_state_data(), {2u, batch_size});
                     auto isa = xt::adapt(init_state.data(), {2u, batch_size});
 
-                    auto &oc = ta.step(true);
+                    {
+                        auto &oc = ta.step(true);
 
-                    for (auto i = 0u; i < batch_size; ++i) {
-                        auto ret = xt::eval(xt::zeros<double>({2u}));
+                        for (auto i = 0u; i < batch_size; ++i) {
+                            auto ret = xt::eval(xt::zeros<double>({2u}));
 
-                        horner_eval(ret, xt::view(tca, xt::all(), xt::all(), i), static_cast<int>(ta.get_order()), 0);
-                        REQUIRE(ret[0] == approximately(xt::view(isa, 0, i)[0], 10.));
-                        REQUIRE(ret[1] == approximately(xt::view(isa, 1, i)[0], 10.));
+                            horner_eval(ret, xt::view(tca, xt::all(), xt::all(), i), static_cast<int>(ta.get_order()),
+                                        0);
+                            REQUIRE(ret[0] == approximately(xt::view(isa, 0, i)[0], 10.));
+                            REQUIRE(ret[1] == approximately(xt::view(isa, 1, i)[0], 10.));
 
-                        horner_eval(ret, xt::view(tca, xt::all(), xt::all(), i), static_cast<int>(ta.get_order()),
-                                    std::get<1>(oc[i]));
-                        REQUIRE(ret[0] == approximately(xt::view(sa, 0, i)[0], 10.));
-                        REQUIRE(ret[1] == approximately(xt::view(sa, 1, i)[0], 10.));
+                            horner_eval(ret, xt::view(tca, xt::all(), xt::all(), i), static_cast<int>(ta.get_order()),
+                                        std::get<1>(oc[i]));
+                            REQUIRE(ret[0] == approximately(xt::view(sa, 0, i)[0], 10.));
+                            REQUIRE(ret[1] == approximately(xt::view(sa, 1, i)[0], 10.));
+                        }
+                    }
+
+                    init_state = ta.get_state();
+
+                    {
+                        auto &oc = ta.step_backward(true);
+
+                        for (auto i = 0u; i < batch_size; ++i) {
+                            auto ret = xt::eval(xt::zeros<double>({2u}));
+
+                            horner_eval(ret, xt::view(tca, xt::all(), xt::all(), i), static_cast<int>(ta.get_order()),
+                                        0);
+                            REQUIRE(ret[0] == approximately(xt::view(isa, 0, i)[0], 10.));
+                            REQUIRE(ret[1] == approximately(xt::view(isa, 1, i)[0], 10.));
+
+                            horner_eval(ret, xt::view(tca, xt::all(), xt::all(), i), static_cast<int>(ta.get_order()),
+                                        std::get<1>(oc[i]));
+                            REQUIRE(ret[0] == approximately(xt::view(sa, 0, i)[0], 10.));
+                            REQUIRE(ret[1] == approximately(xt::view(sa, 1, i)[0], 10.));
+                        }
+                    }
+
+                    init_state = ta.get_state();
+
+                    {
+                        std::vector<double> max_delta_t;
+                        for (auto i = 0u; i < batch_size; ++i) {
+                            max_delta_t.push_back(1e-5 + i * 1e-5);
+                        }
+
+                        auto &oc = ta.step(max_delta_t, true);
+
+                        for (auto i = 0u; i < batch_size; ++i) {
+                            auto ret = xt::eval(xt::zeros<double>({2u}));
+
+                            horner_eval(ret, xt::view(tca, xt::all(), xt::all(), i), static_cast<int>(ta.get_order()),
+                                        0);
+                            REQUIRE(ret[0] == approximately(xt::view(isa, 0, i)[0], 10.));
+                            REQUIRE(ret[1] == approximately(xt::view(isa, 1, i)[0], 10.));
+
+                            horner_eval(ret, xt::view(tca, xt::all(), xt::all(), i), static_cast<int>(ta.get_order()),
+                                        std::get<1>(oc[i]));
+                            REQUIRE(ret[0] == approximately(xt::view(sa, 0, i)[0], 10.));
+                            REQUIRE(ret[1] == approximately(xt::view(sa, 1, i)[0], 10.));
+                        }
                     }
                 }
             }
