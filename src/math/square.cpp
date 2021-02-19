699e04e430dcdce9d18f4d687dfbde511dc33603
Francesco Biscani <bluescarni@gmail.com>
2021-02-19 22:50:43 +0100

    Header fixes.

--- src/math/square.cpp
+++ src/math/square.cpp
@@ -17,11 +17,12 @@
 
 #include <fmt/format.h>
 
-#include <llvm/IR/Attributes.h>
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
 #include <llvm/IR/Type.h>
 #include <llvm/IR/Value.h>
 
