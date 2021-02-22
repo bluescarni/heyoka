6dd99d989643331bfbd0ebbd8b761bedb1199486
Francesco Biscani <bluescarni@gmail.com>
2021-02-22 15:36:53 +0100

    Simplify cos(-x) -> cos(x).

---
+++ test/cos.cpp
@@ -0,0 +1,26 @@
+// Copyright 2020, 2021 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <sstream>
+
+#include <heyoka/expression.hpp>
+#include <heyoka/math/cos.hpp>
+#include <heyoka/math/neg.hpp>
+
+#include "catch.hpp"
+
+using namespace heyoka;
+
+TEST_CASE("cos neg simpl")
+{
+    auto [x, y] = make_vars("x", "y");
+
+    REQUIRE(cos(-(x + y)) == cos(x + y));
+    REQUIRE(cos(neg(x + y)) == cos(x + y));
+    REQUIRE(cos(neg(neg(x + y))) == cos(x + y));
+}
