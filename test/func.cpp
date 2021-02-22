44634bee36c7b7e4b7f480d6c39b81179b59c7ee
Francesco Biscani <bluescarni@gmail.com>
2021-02-22 16:29:56 +0100

    Another tentative MSVC fix.

--- test/func.cpp
+++ test/func.cpp
@@ -681,11 +681,10 @@ TEST_CASE("func extract")
     REQUIRE(f1.extract<func_16>() == nullptr);
     REQUIRE(static_cast<const func &>(f1).extract<func_16>() == nullptr);
 
+#if !defined(_MSC_VER) || defined(__clang__)
+    // NOTE: vanilla MSVC does not like these extraction.
     REQUIRE(f1.extract<const func_15>() == nullptr);
     REQUIRE(static_cast<const func &>(f1).extract<const func_15>() == nullptr);
-
-#if !defined(_MSC_VER) || defined(__clang__)
-    // NOTE: vanilla MSVC does not like int extraction.
 
     REQUIRE(f1.extract<int>() == nullptr);
     REQUIRE(static_cast<const func &>(f1).extract<int>() == nullptr);
