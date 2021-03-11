a17bc8f35d8b8da9cfdc5ec8e2d534144e3be702
Francesco Biscani <bluescarni@gmail.com>
2021-03-11 14:29:19 +0100

    Add negative timestep test.

--- test/taylor_event.cpp
+++ test/taylor_event.cpp
@@ -39,6 +39,76 @@ const auto fp_types = std::tuple<double, long double
 #endif
                                  >{};
 
+TEST_CASE("taylor nte negative timestep")
+{
+    auto tester = [](auto fp_x, unsigned opt_level, bool high_accuracy, bool compact_mode) {
+        using fp_t = decltype(fp_x);
+
+        auto [x, v] = make_vars("x", "v");
+
+        using ev_t = typename taylor_adaptive<fp_t>::nt_event;
+
+        auto counter = 0u;
+
+        // In this test, we define two events:
+        // - the velocity is smaller in absolute
+        //   value than a small limit,
+        // - the velocity is exactly zero.
+        // It is likely that both events are going to fire
+        // in the same timestep, with the first event
+        // firing twice. The sequence of events must
+        // be 0 1 0 repeated a few times.
+        auto ta = taylor_adaptive<fp_t>{{prime(x) = v, prime(v) = -9.8 * sin(x)},
+                                        {fp_t(0), fp_t(.25)},
+                                        kw::opt_level = opt_level,
+                                        kw::high_accuracy = high_accuracy,
+                                        kw::compact_mode = compact_mode,
+                                        kw::nt_events
+                                        = {ev_t(v * v - 1e-10,
+                                                [&counter](taylor_adaptive<fp_t> &ta, fp_t t, std::uint32_t idx) {
+                                                    using std::abs;
+
+                                                    REQUIRE(idx == 0u);
+                                                    REQUIRE((counter % 3u == 0u || counter % 3u == 2u));
+
+                                                    ta.update_d_output(t);
+
+                                                    const auto v = ta.get_d_output()[1];
+                                                    REQUIRE(abs(v * v - 1e-10) < std::numeric_limits<fp_t>::epsilon());
+
+                                                    ++counter;
+                                                }),
+                                           ev_t(v, [&counter](taylor_adaptive<fp_t> &ta, fp_t t, std::uint32_t idx) {
+                                               using std::abs;
+
+                                               REQUIRE(idx == 1u);
+                                               REQUIRE((counter % 3u == 1u));
+
+                                               ta.update_d_output(t);
+
+                                               const auto v = ta.get_d_output()[1];
+                                               REQUIRE(abs(v) < std::numeric_limits<fp_t>::epsilon());
+
+                                               ++counter;
+                                           })}};
+
+        for (auto i = 0; i < 20; ++i) {
+            ta.step_backward();
+        }
+
+        REQUIRE(counter == 12u);
+    };
+
+    for (auto cm : {false, true}) {
+        for (auto f : {false, true}) {
+            tuple_for_each(fp_types, [&tester, f, cm](auto x) { tester(x, 0, f, cm); });
+            tuple_for_each(fp_types, [&tester, f, cm](auto x) { tester(x, 1, f, cm); });
+            tuple_for_each(fp_types, [&tester, f, cm](auto x) { tester(x, 2, f, cm); });
+            tuple_for_each(fp_types, [&tester, f, cm](auto x) { tester(x, 3, f, cm); });
+        }
+    }
+}
+
 TEST_CASE("taylor nte multizero")
 {
     auto tester = [](auto fp_x, unsigned opt_level, bool high_accuracy, bool compact_mode) {
