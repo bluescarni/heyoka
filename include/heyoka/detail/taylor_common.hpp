f3125de204a5d68d5f8174b1a9dd74267244e478
Francesco Biscani <bluescarni@gmail.com>
2021-04-30 16:04:46 +0200

    Another missing header.

--- include/heyoka/detail/taylor_common.hpp
+++ include/heyoka/detail/taylor_common.hpp
@@ -20,6 +20,7 @@
 #include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Type.h>
 
