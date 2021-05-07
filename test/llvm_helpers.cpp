7029a9ee8c60c47b20c34c1da2da05b0633a6129
Francesco Biscani <bluescarni@gmail.com>
2021-05-07 17:03:34 +0200

    Restore original version of the code, try the workaround.

--- test/llvm_helpers.cpp
+++ test/llvm_helpers.cpp
@@ -7,7 +7,17 @@
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <cstdint>
+#include <initializer_list>
 #include <stdexcept>
+#include <vector>
+
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
 
 #include <heyoka/detail/llvm_helpers.hpp>
 #include <heyoka/llvm_state.hpp>
@@ -18,10 +28,43 @@ using namespace heyoka;
 
 TEST_CASE("while_loop")
 {
+    using detail::llvm_while_loop;
+
     for (auto opt_level : {0u, 1u, 2u, 3u}) {
         llvm_state s{kw::opt_level = opt_level};
 
-        detail::llvm_while_loop_test0(s);
+        auto &md = s.module();
+        auto &builder = s.builder();
+        auto &context = s.context();
+
+        auto val_t = builder.getInt32Ty();
+
+        std::vector<llvm::Type *> fargs{val_t};
+        auto *ft = llvm::FunctionType::get(val_t, fargs, false);
+        auto *f = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, "count_n", &md);
+
+        auto final_n = f->args().begin();
+
+        builder.SetInsertPoint(llvm::BasicBlock::Create(context, "entry", f));
+
+        auto retval = builder.CreateAlloca(val_t);
+        builder.CreateStore(builder.getInt32(0), retval);
+
+        llvm_while_loop(
+            s, [&]() -> llvm::Value * { return builder.CreateICmpULT(builder.CreateLoad(retval), final_n); },
+            [&]() { builder.CreateStore(builder.CreateAdd(builder.CreateLoad(retval), builder.getInt32(1)), retval); });
+
+        // Return the result.
+        builder.CreateRet(builder.CreateLoad(retval));
+
+        // Verify.
+        s.verify_function(f);
+
+        // Run the optimisation pass.
+        s.optimise();
+
+        // Compile.
+        s.compile();
 
         // Fetch the function pointerr
         auto f_ptr = reinterpret_cast<std::uint32_t (*)(std::uint32_t)>(s.jit_lookup("count_n"));
@@ -37,12 +80,67 @@ TEST_CASE("while_loop")
     {
         llvm_state s;
 
-        REQUIRE_THROWS_AS(detail::llvm_while_loop_test1(s), std::runtime_error);
+        auto &md = s.module();
+        auto &builder = s.builder();
+        auto &context = s.context();
+
+        auto val_t = builder.getInt32Ty();
+
+        std::vector<llvm::Type *> fargs{val_t};
+        auto *ft = llvm::FunctionType::get(val_t, fargs, false);
+        auto *f = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, "count_n", &md);
+
+        builder.SetInsertPoint(llvm::BasicBlock::Create(context, "entry", f));
+
+        auto retval = builder.CreateAlloca(val_t);
+        builder.CreateStore(builder.getInt32(0), retval);
+
+        auto thrower = [&]() {
+            try {
+                llvm_while_loop(
+                    s, [&]() -> llvm::Value * { throw std::runtime_error{"aa"}; }, [&]() {});
+            } catch (...) {
+                f->eraseFromParent();
+
+                throw;
+            }
+        };
+
+        REQUIRE_THROWS_AS(thrower(), std::runtime_error);
     }
 
     {
         llvm_state s;
 
-        REQUIRE_THROWS_AS(detail::llvm_while_loop_test2(s), std::runtime_error);
+        auto &md = s.module();
+        auto &builder = s.builder();
+        auto &context = s.context();
+
+        auto val_t = builder.getInt32Ty();
+
+        std::vector<llvm::Type *> fargs{val_t};
+        auto *ft = llvm::FunctionType::get(val_t, fargs, false);
+        auto *f = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, "count_n", &md);
+
+        auto final_n = f->args().begin();
+
+        builder.SetInsertPoint(llvm::BasicBlock::Create(context, "entry", f));
+
+        auto retval = builder.CreateAlloca(val_t);
+        builder.CreateStore(builder.getInt32(0), retval);
+
+        auto thrower = [&]() {
+            try {
+                llvm_while_loop(
+                    s, [&]() -> llvm::Value * { return builder.CreateICmpULT(builder.CreateLoad(retval), final_n); },
+                    [&]() { throw std::runtime_error{"aa"}; });
+            } catch (...) {
+                f->eraseFromParent();
+
+                throw;
+            }
+        };
+
+        REQUIRE_THROWS_AS(thrower(), std::runtime_error);
     }
 }
