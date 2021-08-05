2cb3c709e2ea863594cd07c0cbdf1a338c180263
Francesco Biscani <bluescarni@gmail.com>
2021-08-05 12:47:02 +0200

    Add some initial testing.

--- test/vsop2013.cpp
+++ test/vsop2013.cpp
@@ -6,7 +6,9 @@
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include <cmath>
 #include <initializer_list>
+#include <vector>
 
 #include <heyoka/celmec/vsop2013.hpp>
 #include <heyoka/expression.hpp>
@@ -16,15 +18,66 @@
 
 using namespace heyoka;
 
-TEST_CASE("basic")
+TEST_CASE("numerical checks")
 {
+    const std::vector dates = {2411545.0, 2415545.0, 2419545.0, 2423545.0, 2427545.0, 2431545.0,
+                               2435545.0, 2439545.0, 2443545.0, 2447545.0, 2451545.0};
+
     auto x = "x"_var;
 
-    auto merc_a = vsop2013_elliptic(1, 1, kw::vsop2013_time = par[0]);
+    // Mercury.
+    {
+        auto merc_a = vsop2013_elliptic(1, 1, kw::vsop2013_time = par[0]);
+        auto ta = taylor_adaptive<double>{{prime(x) = merc_a}, {0.}, kw::compact_mode = true};
 
-    auto ta = taylor_adaptive<double>{{prime(x) = merc_a}, {0.}, kw::compact_mode = true};
+        const std::vector values = {0.3870979635, 0.3870966235, 0.3870965607, 0.3870975307, 0.3870971271, 0.3870990120,
+                                    0.3870991050, 0.3870986764, 0.3870984073, 0.3870985734, 0.3870982122};
 
-    ta.get_pars_data()[0] = (2447545.0 - 2451545.0) / 365250;
+        for (auto i = 0u; i < 11u; ++i) {
+            ta.set_time(0);
+            ta.get_state_data()[0] = 0;
 
-    ta.propagate_until(1);
+            ta.get_pars_data()[0] = (dates[i] - 2451545.0) / 365250;
+            ta.propagate_until(1);
+
+            REQUIRE(std::abs(ta.get_state()[0] - values[i]) < 1e-8);
+        }
+    }
+
+    {
+        auto merc_lam = vsop2013_elliptic(1, 2, kw::vsop2013_time = par[0]);
+        auto ta = taylor_adaptive<double>{{prime(x) = merc_lam}, {0.}, kw::compact_mode = true};
+
+        const std::vector values = {6.2605163414, 2.9331298264, 5.8889006181, 2.5615070697, 5.5172901512, 2.1899138863,
+                                    5.1457263304, 1.8183546988, 4.7741673767, 1.4467914533, 4.4026055470};
+
+        for (auto i = 0u; i < 11u; ++i) {
+            ta.set_time(0);
+            ta.get_state_data()[0] = 0;
+
+            ta.get_pars_data()[0] = (dates[i] - 2451545.0) / 365250;
+            ta.propagate_until(1);
+
+            REQUIRE(std::abs(std::sin(ta.get_state()[0]) - std::sin(values[i])) < 1e-8);
+            REQUIRE(std::abs(std::cos(ta.get_state()[0]) - std::cos(values[i])) < 1e-8);
+        }
+    }
+
+    {
+        auto merc_k = vsop2013_elliptic(1, 3, kw::vsop2013_time = par[0]);
+        auto ta = taylor_adaptive<double>{{prime(x) = merc_k}, {0.}, kw::compact_mode = true};
+
+        const std::vector values = {0.0452614144, 0.0452099977, 0.0451485382, 0.0450934263, 0.0450275900, 0.0449601649,
+                                    0.0448988996, 0.0448363569, 0.0447776649, 0.0447224543, 0.0446647836};
+
+        for (auto i = 0u; i < 11u; ++i) {
+            ta.set_time(0);
+            ta.get_state_data()[0] = 0;
+
+            ta.get_pars_data()[0] = (dates[i] - 2451545.0) / 365250;
+            ta.propagate_until(1);
+
+            REQUIRE(std::abs(ta.get_state()[0] - values[i]) < 1e-8);
+        }
+    }
 }
