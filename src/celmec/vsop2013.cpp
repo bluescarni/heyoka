03e8f0e2c953da8cd742a63c10094c7d34e5dc88
Francesco Biscani <bluescarni@gmail.com>
2021-08-28 23:41:39 +0200

    Experiments with TBB graphs.

--- src/celmec/vsop2013.cpp
+++ src/celmec/vsop2013.cpp
@@ -36,6 +36,7 @@
 #endif
 
 #include <tbb/blocked_range.h>
+#include <tbb/flow_graph.h>
 #include <tbb/parallel_for.h>
 #include <tbb/parallel_invoke.h>
 
@@ -307,6 +308,237 @@ expression vsop2013_elliptic_impl(std::uint32_t pl_idx, std::uint32_t var_idx, e
 // are referred to the Dynamical Frame J2000.
 std::vector<expression> vsop2013_cartesian_impl(std::uint32_t pl_idx, expression t_expr, double thresh)
 {
+#if 0
+    namespace flow = tbb::flow;
+
+    // G*M values for the planets.
+    constexpr double gm_pl[] = {4.9125474514508118699e-11, 7.2434524861627027000e-10, 8.9970116036316091182e-10,
+                                9.5495351057792580598e-11, 2.8253458420837780000e-07, 8.4597151856806587398e-08,
+                                1.2920249167819693900e-08, 1.5243589007842762800e-08, 2.1886997654259696800e-12};
+
+    // G*M value for the Sun.
+    constexpr double gm_sun = 2.9591220836841438269e-04;
+
+    // Compute the gravitational parameter for pl_idx.
+    assert(pl_idx >= 1u && pl_idx <= 9u); // LCOV_EXCL_LINE
+    const auto mu = std::sqrt(gm_sun + gm_pl[pl_idx - 1u]);
+
+    // Prepare the return value.
+    std::vector<expression> retval(6u);
+
+    expression a, lam, k, h, q, p, M, kh_2, qp_2, qp, E, e, sqrt_1me2, ci, si, cOm, sOm, sin_E, cos_E, com, som, q1_a,
+        q2_a, R00, R01, R10, R11, R20, R21, v_num, v_den;
+
+    flow::graph g;
+
+    flow::broadcast_node<flow::continue_msg> start(g);
+
+    // Get the elliptic orbital elements.
+    flow::continue_node<flow::continue_msg> a_node(
+        g, [&](auto) { a = vsop2013_elliptic_impl(pl_idx, 1, t_expr, thresh); });
+    flow::continue_node<flow::continue_msg> lam_node(
+        g, [&](auto) { lam = vsop2013_elliptic_impl(pl_idx, 2, t_expr, thresh); });
+    flow::continue_node<flow::continue_msg> k_node(
+        g, [&](auto) { k = vsop2013_elliptic_impl(pl_idx, 3, t_expr, thresh); });
+    flow::continue_node<flow::continue_msg> h_node(
+        g, [&](auto) { h = vsop2013_elliptic_impl(pl_idx, 4, t_expr, thresh); });
+    flow::continue_node<flow::continue_msg> q_node(
+        g, [&](auto) { q = vsop2013_elliptic_impl(pl_idx, 5, t_expr, thresh); });
+    flow::continue_node<flow::continue_msg> p_node(
+        g, [&](auto) { p = vsop2013_elliptic_impl(pl_idx, 6, t_expr, thresh); });
+    flow::make_edge(start, a_node);
+    flow::make_edge(start, lam_node);
+    flow::make_edge(start, k_node);
+    flow::make_edge(start, h_node);
+    flow::make_edge(start, q_node);
+    flow::make_edge(start, p_node);
+
+    // M.
+    flow::continue_node<flow::continue_msg> M_node(g, [&](auto) { M = lam - atan2(h, k); });
+    flow::make_edge(lam_node, M_node);
+    flow::make_edge(h_node, M_node);
+    flow::make_edge(k_node, M_node);
+
+    // k**2 + h**2.
+    flow::continue_node<flow::continue_msg> kh2_node(g, [&](auto) { kh_2 = square(k) + square(h); });
+    flow::make_edge(k_node, kh2_node);
+    flow::make_edge(h_node, kh2_node);
+
+    // q**2 + p**2.
+    flow::continue_node<flow::continue_msg> qp2_node(g, [&](auto) { qp_2 = square(q) + square(p); });
+    flow::make_edge(q_node, qp2_node);
+    flow::make_edge(p_node, qp2_node);
+
+    // sqrt(q**2 + p**2).
+    flow::continue_node<flow::continue_msg> qp_node(g, [&](auto) { qp = sqrt(qp_2); });
+    flow::make_edge(qp2_node, qp_node);
+
+    // e.
+    flow::continue_node<flow::continue_msg> e_node(g, [&](auto) { e = sqrt(kh_2); });
+    flow::make_edge(kh2_node, e_node);
+
+    // E.
+    flow::continue_node<flow::continue_msg> E_node(g, [&](auto) { E = kepE(e, M); });
+    flow::make_edge(e_node, E_node);
+    flow::make_edge(M_node, E_node);
+
+    // sin(E)/cos(E).
+    flow::continue_node<flow::continue_msg> sin_E_node(g, [&](auto) { sin_E = sin(E); });
+    flow::make_edge(E_node, sin_E_node);
+    flow::continue_node<flow::continue_msg> cos_E_node(g, [&](auto) { cos_E = cos(E); });
+    flow::make_edge(E_node, cos_E_node);
+
+    // sqrt(1 - e**2).
+    flow::continue_node<flow::continue_msg> sqrt_1me2_node(g, [&](auto) { sqrt_1me2 = sqrt(1_dbl - kh_2); });
+    flow::make_edge(kh2_node, sqrt_1me2_node);
+
+    // sin(i)/cos(i).
+    flow::continue_node<flow::continue_msg> ci_node(g, [&](auto) { ci = 1_dbl - 2_dbl * qp_2; });
+    flow::make_edge(qp2_node, ci_node);
+    flow::continue_node<flow::continue_msg> si_node(g, [&](auto) { si = sqrt(1_dbl - square(ci)); });
+    flow::make_edge(ci_node, si_node);
+
+    // cos(Om)/sin(Om).
+    flow::continue_node<flow::continue_msg> cOm_node(g, [&](auto) { cOm = q / qp; });
+    flow::make_edge(q_node, cOm_node);
+    flow::make_edge(qp_node, cOm_node);
+    flow::continue_node<flow::continue_msg> sOm_node(g, [&](auto) { sOm = p / qp; });
+    flow::make_edge(p_node, sOm_node);
+    flow::make_edge(qp_node, sOm_node);
+
+    // cos(om)/sin(om).
+    flow::continue_node<flow::continue_msg> com_node(g, [&](auto) { com = (k * cOm + h * sOm) / e; });
+    flow::make_edge(k_node, com_node);
+    flow::make_edge(cOm_node, com_node);
+    flow::make_edge(h_node, com_node);
+    flow::make_edge(sOm_node, com_node);
+    flow::make_edge(e_node, com_node);
+    flow::continue_node<flow::continue_msg> som_node(g, [&](auto) { som = (h * cOm - k * sOm) / e; });
+    flow::make_edge(h_node, som_node);
+    flow::make_edge(cOm_node, som_node);
+    flow::make_edge(k_node, som_node);
+    flow::make_edge(sOm_node, som_node);
+    flow::make_edge(e_node, som_node);
+
+    // q1/a.
+    flow::continue_node<flow::continue_msg> q1_a_node(g, [&](auto) { q1_a = cos_E - e; });
+    flow::make_edge(cos_E_node, q1_a_node);
+    flow::make_edge(e_node, q1_a_node);
+
+    // q2/a
+    flow::continue_node<flow::continue_msg> q2_a_node(g, [&](auto) { q2_a = sqrt_1me2 * sin_E; });
+    flow::make_edge(sqrt_1me2_node, q2_a_node);
+    flow::make_edge(sin_E_node, q2_a_node);
+
+    // R00.
+    flow::continue_node<flow::continue_msg> R00_node(g, [&](auto) { R00 = cOm * com - sOm * ci * som; });
+    flow::make_edge(cOm_node, R00_node);
+    flow::make_edge(com_node, R00_node);
+    flow::make_edge(sOm_node, R00_node);
+    flow::make_edge(ci_node, R00_node);
+    flow::make_edge(som_node, R00_node);
+
+    // R01.
+    flow::continue_node<flow::continue_msg> R01_node(g, [&](auto) { R01 = cOm * som + sOm * ci * com; });
+    flow::make_edge(cOm_node, R01_node);
+    flow::make_edge(som_node, R01_node);
+    flow::make_edge(sOm_node, R01_node);
+    flow::make_edge(ci_node, R01_node);
+    flow::make_edge(com_node, R01_node);
+
+    // R10.
+    flow::continue_node<flow::continue_msg> R10_node(g, [&](auto) { R10 = sOm * com + cOm * ci * som; });
+    flow::make_edge(sOm_node, R10_node);
+    flow::make_edge(com_node, R10_node);
+    flow::make_edge(cOm_node, R10_node);
+    flow::make_edge(ci_node, R10_node);
+    flow::make_edge(som_node, R10_node);
+
+    // R11.
+    flow::continue_node<flow::continue_msg> R11_node(g, [&](auto) { R11 = sOm * som - cOm * ci * com; });
+    flow::make_edge(sOm_node, R11_node);
+    flow::make_edge(som_node, R11_node);
+    flow::make_edge(cOm_node, R11_node);
+    flow::make_edge(ci_node, R11_node);
+    flow::make_edge(com_node, R11_node);
+
+    // R20.
+    flow::continue_node<flow::continue_msg> R20_node(g, [&](auto) { R20 = si * som; });
+    flow::make_edge(si_node, R20_node);
+    flow::make_edge(som_node, R20_node);
+
+    // R21.
+    flow::continue_node<flow::continue_msg> R21_node(g, [&](auto) { R21 = si * com; });
+    flow::make_edge(si_node, R21_node);
+    flow::make_edge(com_node, R21_node);
+
+    // v_num.
+    flow::continue_node<flow::continue_msg> v_num_node(g, [&](auto) { v_num = sqrt_1me2 * cos_E; });
+    flow::make_edge(sqrt_1me2_node, v_num_node);
+    flow::make_edge(cos_E_node, v_num_node);
+
+    // v_den.
+    flow::continue_node<flow::continue_msg> v_den_node(g, [&](auto) { v_den = sqrt(a) * (1_dbl - e * cos_E); });
+    flow::make_edge(a_node, v_den_node);
+    flow::make_edge(e_node, v_den_node);
+    flow::make_edge(cos_E_node, v_den_node);
+
+    // x.
+    flow::continue_node<flow::continue_msg> x_node(g, [&](auto) { retval[0] = a * (q1_a * R00 - q2_a * R01); });
+    flow::make_edge(a_node, x_node);
+    flow::make_edge(q1_a_node, x_node);
+    flow::make_edge(R00_node, x_node);
+    flow::make_edge(q2_a_node, x_node);
+    flow::make_edge(R01_node, x_node);
+
+    // y.
+    flow::continue_node<flow::continue_msg> y_node(g, [&](auto) { retval[1] = a * (q1_a * R10 - q2_a * R11); });
+    flow::make_edge(a_node, y_node);
+    flow::make_edge(q1_a_node, y_node);
+    flow::make_edge(R10_node, y_node);
+    flow::make_edge(q2_a_node, y_node);
+    flow::make_edge(R11_node, y_node);
+
+    // z.
+    flow::continue_node<flow::continue_msg> z_node(g, [&](auto) { retval[2] = a * (q1_a * R20 + q2_a * R21); });
+    flow::make_edge(a_node, z_node);
+    flow::make_edge(q1_a_node, z_node);
+    flow::make_edge(R20_node, z_node);
+    flow::make_edge(q2_a_node, z_node);
+    flow::make_edge(R21_node, z_node);
+
+    // vx.
+    flow::continue_node<flow::continue_msg> vx_node(
+        g, [&](auto) { retval[3] = mu * (-sin_E * R00 - v_num * R01) / v_den; });
+    flow::make_edge(sin_E_node, vx_node);
+    flow::make_edge(R00_node, vx_node);
+    flow::make_edge(v_num_node, vx_node);
+    flow::make_edge(R01_node, vx_node);
+    flow::make_edge(v_den_node, vx_node);
+
+    // vy.
+    flow::continue_node<flow::continue_msg> vy_node(
+        g, [&](auto) { retval[4] = mu * (-sin_E * R10 - v_num * R11) / v_den; });
+    flow::make_edge(sin_E_node, vy_node);
+    flow::make_edge(R10_node, vy_node);
+    flow::make_edge(v_num_node, vy_node);
+    flow::make_edge(R11_node, vy_node);
+    flow::make_edge(v_den_node, vy_node);
+
+    // vz.
+    flow::continue_node<flow::continue_msg> vz_node(
+        g, [&](auto) { retval[5] = mu * (-sin_E * R20 + v_num * R21) / v_den; });
+    flow::make_edge(sin_E_node, vz_node);
+    flow::make_edge(R20_node, vz_node);
+    flow::make_edge(v_num_node, vz_node);
+    flow::make_edge(R21_node, vz_node);
+    flow::make_edge(v_den_node, vz_node);
+
+    start.try_put(flow::continue_msg{});
+    g.wait_for_all();
+
+    return retval;
+#else
     // Get the elliptic orbital elements.
     expression a, lam, k, h, q, p;
 
@@ -375,6 +607,7 @@ std::vector<expression> vsop2013_cartesian_impl(std::uint32_t pl_idx, expression
                          [&]() { retval[5] = mu * (-sin_E * R20 + v_num * R21) / v_den; });
 
     return retval;
+#endif
 }
 
 // Implementation of the function constructing the VSOP2013 Cartesian series as heyoka expressions. The coordinates
