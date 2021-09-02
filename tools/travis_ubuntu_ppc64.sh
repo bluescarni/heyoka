58e3a227c6a29422b370c4dadb982836c3556640
Francesco Biscani <bluescarni@gmail.com>
2021-09-02 13:26:43 +0200

    Another script fix. [skip appveyor]

--- tools/travis_ubuntu_ppc64.sh
+++ tools/travis_ubuntu_ppc64.sh
@@ -9,6 +9,7 @@ set -e
 # Core deps.
 #apt-get update
 #apt-get -y install wget
+yum -y install wget
 
 # Install conda+deps.
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-ppc64le.sh -O miniconda.sh
