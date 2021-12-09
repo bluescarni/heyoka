ef6b021cd92053e2287cbf910eedaeaae8feef9d
Francesco Biscani <bluescarni@gmail.com>
2021-12-09 14:53:21 +0100

    Ensemble propagate_grid(), scalar and batch.

--- include/heyoka/ensemble_propagate.hpp
+++ include/heyoka/ensemble_propagate.hpp
@@ -114,6 +114,45 @@ ensemble_propagate_for_impl<mppp::real128>(
 
 #endif
 
+template <typename T>
+inline std::vector<std::tuple<taylor_adaptive<T>, taylor_outcome, T, T, std::size_t, std::vector<T>>>
+ensemble_propagate_grid_impl(const taylor_adaptive<T> &, const std::vector<T> &, std::size_t,
+                             const std::function<taylor_adaptive<T>(taylor_adaptive<T>, std::size_t)> &, std::size_t, T,
+                             const std::function<bool(taylor_adaptive<T> &)> &)
+{
+    static_assert(detail::always_false_v<T>, "Unhandled type.");
+
+    throw;
+}
+
+template <>
+HEYOKA_DLL_PUBLIC
+    std::vector<std::tuple<taylor_adaptive<double>, taylor_outcome, double, double, std::size_t, std::vector<double>>>
+    ensemble_propagate_grid_impl<double>(
+        const taylor_adaptive<double> &, const std::vector<double> &, std::size_t,
+        const std::function<taylor_adaptive<double>(taylor_adaptive<double>, std::size_t)> &, std::size_t, double,
+        const std::function<bool(taylor_adaptive<double> &)> &);
+
+template <>
+HEYOKA_DLL_PUBLIC std::vector<std::tuple<taylor_adaptive<long double>, taylor_outcome, long double, long double,
+                                         std::size_t, std::vector<long double>>>
+ensemble_propagate_grid_impl<long double>(
+    const taylor_adaptive<long double> &, const std::vector<long double> &, std::size_t,
+    const std::function<taylor_adaptive<long double>(taylor_adaptive<long double>, std::size_t)> &, std::size_t,
+    long double, const std::function<bool(taylor_adaptive<long double> &)> &);
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+template <>
+HEYOKA_DLL_PUBLIC std::vector<std::tuple<taylor_adaptive<mppp::real128>, taylor_outcome, mppp::real128, mppp::real128,
+                                         std::size_t, std::vector<mppp::real128>>>
+ensemble_propagate_grid_impl<mppp::real128>(
+    const taylor_adaptive<mppp::real128> &, const std::vector<mppp::real128> &, std::size_t,
+    const std::function<taylor_adaptive<mppp::real128>(taylor_adaptive<mppp::real128>, std::size_t)> &, std::size_t,
+    mppp::real128, const std::function<bool(taylor_adaptive<mppp::real128> &)> &);
+
+#endif
+
 } // namespace detail
 
 template <typename T, typename... KwArgs>
@@ -141,6 +180,18 @@ ensemble_propagate_for(const taylor_adaptive<T> &ta, T delta_t, std::size_t n_it
 
     return detail::ensemble_propagate_for_impl(ta, delta_t, n_iter, gen, max_steps, max_delta_t, cb, write_tc,
                                                with_c_out);
+}
+
+template <typename T, typename... KwArgs>
+inline std::vector<std::tuple<taylor_adaptive<T>, taylor_outcome, T, T, std::size_t, std::vector<T>>>
+ensemble_propagate_grid(const taylor_adaptive<T> &ta, const std::vector<T> &grid, std::size_t n_iter,
+                        const std::function<taylor_adaptive<T>(taylor_adaptive<T>, std::size_t)> &gen,
+                        KwArgs &&...kw_args)
+{
+    auto [max_steps, max_delta_t, cb, _]
+        = detail::taylor_propagate_common_ops<T, true>(std::forward<KwArgs>(kw_args)...);
+
+    return detail::ensemble_propagate_grid_impl(ta, grid, n_iter, gen, max_steps, max_delta_t, cb);
 }
 
 namespace detail
@@ -228,6 +279,43 @@ HEYOKA_DLL_PUBLIC
 
 #endif
 
+template <typename T>
+inline std::vector<std::tuple<taylor_adaptive_batch<T>, std::vector<T>>> ensemble_propagate_grid_batch_impl(
+    const taylor_adaptive_batch<T> &, const std::vector<T> &, std::size_t,
+    const std::function<taylor_adaptive_batch<T>(taylor_adaptive_batch<T>, std::size_t)> &, std::size_t,
+    const std::vector<T> &, const std::function<bool(taylor_adaptive_batch<T> &)> &)
+{
+    static_assert(detail::always_false_v<T>, "Unhandled type.");
+
+    throw;
+}
+
+template <>
+HEYOKA_DLL_PUBLIC std::vector<std::tuple<taylor_adaptive_batch<double>, std::vector<double>>>
+ensemble_propagate_grid_batch_impl<double>(
+    const taylor_adaptive_batch<double> &, const std::vector<double> &, std::size_t,
+    const std::function<taylor_adaptive_batch<double>(taylor_adaptive_batch<double>, std::size_t)> &, std::size_t,
+    const std::vector<double> &, const std::function<bool(taylor_adaptive_batch<double> &)> &);
+
+template <>
+HEYOKA_DLL_PUBLIC std::vector<std::tuple<taylor_adaptive_batch<long double>, std::vector<long double>>>
+ensemble_propagate_grid_batch_impl<long double>(
+    const taylor_adaptive_batch<long double> &, const std::vector<long double> &, std::size_t,
+    const std::function<taylor_adaptive_batch<long double>(taylor_adaptive_batch<long double>, std::size_t)> &,
+    std::size_t, const std::vector<long double> &, const std::function<bool(taylor_adaptive_batch<long double> &)> &);
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+template <>
+HEYOKA_DLL_PUBLIC std::vector<std::tuple<taylor_adaptive_batch<mppp::real128>, std::vector<mppp::real128>>>
+ensemble_propagate_grid_batch_impl<mppp::real128>(
+    const taylor_adaptive_batch<mppp::real128> &, const std::vector<mppp::real128> &, std::size_t,
+    const std::function<taylor_adaptive_batch<mppp::real128>(taylor_adaptive_batch<mppp::real128>, std::size_t)> &,
+    std::size_t, const std::vector<mppp::real128> &,
+    const std::function<bool(taylor_adaptive_batch<mppp::real128> &)> &);
+
+#endif
+
 } // namespace detail
 
 template <typename T, typename... KwArgs>
@@ -237,7 +325,8 @@ ensemble_propagate_until_batch(
     const std::function<taylor_adaptive_batch<T>(taylor_adaptive_batch<T>, std::size_t)> &gen, KwArgs &&...kw_args)
 {
     auto [max_steps, max_delta_ts, cb, write_tc, with_c_out]
-        = detail::taylor_propagate_common_ops_batch<T, false>(std::forward<KwArgs>(kw_args)...);
+        = detail::taylor_propagate_common_ops_batch<T, false, true>(ta.get_batch_size(),
+                                                                    std::forward<KwArgs>(kw_args)...);
 
     return detail::ensemble_propagate_until_batch_impl(ta, t, n_iter, gen, max_steps, max_delta_ts, cb, write_tc,
                                                        with_c_out);
@@ -250,10 +339,23 @@ ensemble_propagate_for_batch(const taylor_adaptive_batch<T> &ta, T delta_t, std:
                              KwArgs &&...kw_args)
 {
     auto [max_steps, max_delta_ts, cb, write_tc, with_c_out]
-        = detail::taylor_propagate_common_ops_batch<T, false>(std::forward<KwArgs>(kw_args)...);
+        = detail::taylor_propagate_common_ops_batch<T, false, true>(ta.get_batch_size(),
+                                                                    std::forward<KwArgs>(kw_args)...);
 
     return detail::ensemble_propagate_for_batch_impl(ta, delta_t, n_iter, gen, max_steps, max_delta_ts, cb, write_tc,
                                                      with_c_out);
+}
+
+template <typename T, typename... KwArgs>
+inline std::vector<std::tuple<taylor_adaptive_batch<T>, std::vector<T>>>
+ensemble_propagate_grid_batch(const taylor_adaptive_batch<T> &ta, const std::vector<T> &grid, std::size_t n_iter,
+                              const std::function<taylor_adaptive_batch<T>(taylor_adaptive_batch<T>, std::size_t)> &gen,
+                              KwArgs &&...kw_args)
+{
+    auto [max_steps, max_delta_ts, cb, _] = detail::taylor_propagate_common_ops_batch<T, true, true>(
+        ta.get_batch_size(), std::forward<KwArgs>(kw_args)...);
+
+    return detail::ensemble_propagate_grid_batch_impl(ta, grid, n_iter, gen, max_steps, max_delta_ts, cb);
 }
 
 } // namespace heyoka
--- src/ensemble_propagate.cpp
+++ src/ensemble_propagate.cpp
@@ -8,9 +8,13 @@
 
 #include <heyoka/config.hpp>
 
+#include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <functional>
+#include <limits>
 #include <optional>
+#include <stdexcept>
 #include <tuple>
 #include <utility>
 #include <vector>
@@ -132,6 +136,48 @@ ensemble_propagate_for_generic(const taylor_adaptive<T> &ta, T delta_t, std::siz
     return retval;
 }
 
+template <typename T>
+std::vector<std::tuple<taylor_adaptive<T>, taylor_outcome, T, T, std::size_t, std::vector<T>>>
+ensemble_propagate_grid_generic(const taylor_adaptive<T> &ta, const std::vector<T> &grid, std::size_t n_iter,
+                                const std::function<taylor_adaptive<T>(taylor_adaptive<T>, std::size_t)> &gen,
+                                std::size_t max_steps, T max_delta_t,
+                                const std::function<bool(taylor_adaptive<T> &)> &cb)
+{
+    // NOTE: store the results into a vector of optionals, so that we avoid
+    // having to init a large number of default-constructed integrators
+    // that are anyway going to be destroyed.
+    std::vector<std::optional<std::tuple<taylor_adaptive<T>, taylor_outcome, T, T, std::size_t, std::vector<T>>>>
+        opt_retval;
+    opt_retval.resize(boost::numeric_cast<decltype(opt_retval.size())>(n_iter));
+
+    // The actual return value, into which we will eventually move the results of the
+    // integrations.
+    std::vector<std::tuple<taylor_adaptive<T>, taylor_outcome, T, T, std::size_t, std::vector<T>>> retval;
+    retval.reserve(boost::numeric_cast<decltype(retval.size())>(n_iter));
+
+    tbb::parallel_for(tbb::blocked_range<std::size_t>(0u, n_iter), [&](const auto &range) {
+        for (auto i = range.begin(); i != range.end(); ++i) {
+            // Generate the integrator for the current iteration.
+            auto local_ta = gen(ta, i);
+
+            // Do the propagation.
+            auto loc_ret = local_ta.propagate_grid(grid, kw::max_steps = max_steps, kw::max_delta_t = max_delta_t,
+                                                   kw::callback = cb);
+
+            // Assign the results.
+            opt_retval[i].emplace(std::move(local_ta), std::get<0>(loc_ret), std::get<1>(loc_ret), std::get<2>(loc_ret),
+                                  std::get<3>(loc_ret), std::move(std::get<4>(loc_ret)));
+        }
+    });
+
+    // Move the results from opt_retval to retval.
+    for (auto &opt : opt_retval) {
+        retval.push_back(std::move(*opt));
+    }
+
+    return retval;
+}
+
 } // namespace
 
 template <>
@@ -208,6 +254,42 @@ ensemble_propagate_for_impl<mppp::real128>(
     bool write_tc, bool with_c_out)
 {
     return ensemble_propagate_for_generic(ta, delta_t, n_iter, gen, max_steps, max_delta_t, cb, write_tc, with_c_out);
+}
+
+#endif
+
+template <>
+std::vector<std::tuple<taylor_adaptive<double>, taylor_outcome, double, double, std::size_t, std::vector<double>>>
+ensemble_propagate_grid_impl<double>(
+    const taylor_adaptive<double> &ta, const std::vector<double> &grid, std::size_t n_iter,
+    const std::function<taylor_adaptive<double>(taylor_adaptive<double>, std::size_t)> &gen, std::size_t max_steps,
+    double max_delta_t, const std::function<bool(taylor_adaptive<double> &)> &cb)
+{
+    return ensemble_propagate_grid_generic(ta, grid, n_iter, gen, max_steps, max_delta_t, cb);
+}
+
+template <>
+std::vector<std::tuple<taylor_adaptive<long double>, taylor_outcome, long double, long double, std::size_t,
+                       std::vector<long double>>>
+ensemble_propagate_grid_impl<long double>(
+    const taylor_adaptive<long double> &ta, const std::vector<long double> &grid, std::size_t n_iter,
+    const std::function<taylor_adaptive<long double>(taylor_adaptive<long double>, std::size_t)> &gen,
+    std::size_t max_steps, long double max_delta_t, const std::function<bool(taylor_adaptive<long double> &)> &cb)
+{
+    return ensemble_propagate_grid_generic(ta, grid, n_iter, gen, max_steps, max_delta_t, cb);
+}
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+template <>
+std::vector<std::tuple<taylor_adaptive<mppp::real128>, taylor_outcome, mppp::real128, mppp::real128, std::size_t,
+                       std::vector<mppp::real128>>>
+ensemble_propagate_grid_impl<mppp::real128>(
+    const taylor_adaptive<mppp::real128> &ta, const std::vector<mppp::real128> &grid, std::size_t n_iter,
+    const std::function<taylor_adaptive<mppp::real128>(taylor_adaptive<mppp::real128>, std::size_t)> &gen,
+    std::size_t max_steps, mppp::real128 max_delta_t, const std::function<bool(taylor_adaptive<mppp::real128> &)> &cb)
+{
+    return ensemble_propagate_grid_generic(ta, grid, n_iter, gen, max_steps, max_delta_t, cb);
 }
 
 #endif
@@ -301,6 +383,63 @@ ensemble_propagate_for_batch_generic(
     return retval;
 }
 
+template <typename T>
+std::vector<std::tuple<taylor_adaptive_batch<T>, std::vector<T>>> ensemble_propagate_grid_batch_generic(
+    const taylor_adaptive_batch<T> &ta, const std::vector<T> &grid_, std::size_t n_iter,
+    const std::function<taylor_adaptive_batch<T>(taylor_adaptive_batch<T>, std::size_t)> &gen, std::size_t max_steps,
+    const std::vector<T> &max_delta_ts, const std::function<bool(taylor_adaptive_batch<T> &)> &cb)
+{
+    const auto batch_size = ta.get_batch_size();
+    assert(batch_size != 0u); // LCOV_EXCL_LINE
+
+    // Splat out the time grid.
+    // LCOV_EXCL_START
+    if (grid_.size() > std::numeric_limits<decltype(grid_.size())>::max() / batch_size) {
+        throw std::overflow_error("Overflow detected in an ensemble propagation");
+    }
+    // LCOV_EXCL_STOP
+    std::vector<T> grid;
+    grid.reserve(grid_.size() * batch_size);
+    for (auto gval : grid_) {
+        for (std::uint32_t i = 0; i < batch_size; ++i) {
+            grid.push_back(gval);
+        }
+    }
+    assert(grid.size() == grid_.size() * batch_size); // LCOV_EXCL_LINE
+
+    // NOTE: store the results into a vector of optionals, so that we avoid
+    // having to init a large number of default-constructed integrators
+    // that are anyway going to be destroyed.
+    std::vector<std::optional<std::tuple<taylor_adaptive_batch<T>, std::vector<T>>>> opt_retval;
+    opt_retval.resize(boost::numeric_cast<decltype(opt_retval.size())>(n_iter));
+
+    // The actual return value, into which we will eventually move the results of the
+    // integrations.
+    std::vector<std::tuple<taylor_adaptive_batch<T>, std::vector<T>>> retval;
+    retval.reserve(boost::numeric_cast<decltype(retval.size())>(n_iter));
+
+    tbb::parallel_for(tbb::blocked_range<std::size_t>(0u, n_iter), [&](const auto &range) {
+        for (auto i = range.begin(); i != range.end(); ++i) {
+            // Generate the integrator for the current iteration.
+            auto local_ta = gen(ta, i);
+
+            // Do the propagation.
+            auto loc_ret = local_ta.propagate_grid(grid, kw::max_steps = max_steps, kw::max_delta_t = max_delta_ts,
+                                                   kw::callback = cb);
+
+            // Assign the results.
+            opt_retval[i].emplace(std::move(local_ta), std::move(loc_ret));
+        }
+    });
+
+    // Move the results from opt_retval to retval.
+    for (auto &opt : opt_retval) {
+        retval.push_back(std::move(*opt));
+    }
+
+    return retval;
+}
+
 } // namespace
 
 template <>
@@ -379,6 +518,42 @@ ensemble_propagate_for_batch_impl<mppp::real128>(
 {
     return ensemble_propagate_for_batch_generic(ta, delta_t, n_iter, gen, max_steps, max_delta_ts, cb, write_tc,
                                                 with_c_out);
+}
+
+#endif
+
+template <>
+std::vector<std::tuple<taylor_adaptive_batch<double>, std::vector<double>>> ensemble_propagate_grid_batch_impl<double>(
+    const taylor_adaptive_batch<double> &ta, const std::vector<double> &grid, std::size_t n_iter,
+    const std::function<taylor_adaptive_batch<double>(taylor_adaptive_batch<double>, std::size_t)> &gen,
+    std::size_t max_steps, const std::vector<double> &max_delta_ts,
+    const std::function<bool(taylor_adaptive_batch<double> &)> &cb)
+{
+    return ensemble_propagate_grid_batch_generic(ta, grid, n_iter, gen, max_steps, max_delta_ts, cb);
+}
+
+template <>
+std::vector<std::tuple<taylor_adaptive_batch<long double>, std::vector<long double>>>
+ensemble_propagate_grid_batch_impl<long double>(
+    const taylor_adaptive_batch<long double> &ta, const std::vector<long double> &grid, std::size_t n_iter,
+    const std::function<taylor_adaptive_batch<long double>(taylor_adaptive_batch<long double>, std::size_t)> &gen,
+    std::size_t max_steps, const std::vector<long double> &max_delta_ts,
+    const std::function<bool(taylor_adaptive_batch<long double> &)> &cb)
+{
+    return ensemble_propagate_grid_batch_generic(ta, grid, n_iter, gen, max_steps, max_delta_ts, cb);
+}
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+template <>
+std::vector<std::tuple<taylor_adaptive_batch<mppp::real128>, std::vector<mppp::real128>>>
+ensemble_propagate_grid_batch_impl<mppp::real128>(
+    const taylor_adaptive_batch<mppp::real128> &ta, const std::vector<mppp::real128> &grid, std::size_t n_iter,
+    const std::function<taylor_adaptive_batch<mppp::real128>(taylor_adaptive_batch<mppp::real128>, std::size_t)> &gen,
+    std::size_t max_steps, const std::vector<mppp::real128> &max_delta_ts,
+    const std::function<bool(taylor_adaptive_batch<mppp::real128> &)> &cb)
+{
+    return ensemble_propagate_grid_batch_generic(ta, grid, n_iter, gen, max_steps, max_delta_ts, cb);
 }
 
 #endif
