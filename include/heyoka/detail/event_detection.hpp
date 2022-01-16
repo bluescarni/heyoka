d4d02d8f147c67b4a7a4b4d732037036db9b0fd9
Francesco Biscani <bluescarni@gmail.com>
2022-01-16 10:39:58 +0100

    Make public a function used during event detection.

--- include/heyoka/detail/event_detection.hpp
+++ include/heyoka/detail/event_detection.hpp
@@ -47,8 +47,7 @@ mppp::real128 taylor_deduce_cooldown(mppp::real128, mppp::real128);
 
 #endif
 
-// Machinery to add a fast event exclusion check function to an llvm_state. Public for
-// testing purposes.
+// Machinery to add a fast event exclusion check function to an llvm_state.
 HEYOKA_DLL_PUBLIC llvm::Function *llvm_add_fex_check_dbl(llvm_state &, std::uint32_t, std::uint32_t);
 HEYOKA_DLL_PUBLIC llvm::Function *llvm_add_fex_check_ldbl(llvm_state &, std::uint32_t, std::uint32_t);
 
@@ -68,6 +67,37 @@ inline llvm::Function *llvm_add_fex_check(llvm_state &s, std::uint32_t n, std::u
 #if defined(HEYOKA_HAVE_REAL128)
     } else if constexpr (std::is_same_v<T, mppp::real128>) {
         return llvm_add_fex_check_f128(s, n, batch_size);
+#endif
+    } else {
+        static_assert(always_false_v<T>, "Unhandled type.");
+    }
+}
+
+// Machinery to add a function that, given an input polynomial of order n represented
+// as an array of coefficients:
+// - reverses it,
+// - translates it by 1,
+// - counts the sign changes in the coefficients
+//   of the resulting polynomial.
+HEYOKA_DLL_PUBLIC llvm::Function *llvm_add_poly_rtscc_dbl(llvm_state &, std::uint32_t, std::uint32_t);
+HEYOKA_DLL_PUBLIC llvm::Function *llvm_add_poly_rtscc_ldbl(llvm_state &, std::uint32_t, std::uint32_t);
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+HEYOKA_DLL_PUBLIC llvm::Function *llvm_add_poly_rtscc_f128(llvm_state &, std::uint32_t, std::uint32_t);
+
+#endif
+
+template <typename T>
+inline llvm::Function *llvm_add_poly_rtscc(llvm_state &s, std::uint32_t n, std::uint32_t batch_size)
+{
+    if constexpr (std::is_same_v<T, double>) {
+        return llvm_add_poly_rtscc_dbl(s, n, batch_size);
+    } else if constexpr (std::is_same_v<T, long double>) {
+        return llvm_add_poly_rtscc_ldbl(s, n, batch_size);
+#if defined(HEYOKA_HAVE_REAL128)
+    } else if constexpr (std::is_same_v<T, mppp::real128>) {
+        return llvm_add_poly_rtscc_f128(s, n, batch_size);
 #endif
     } else {
         static_assert(always_false_v<T>, "Unhandled type.");
