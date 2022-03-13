935a261f47df0f037fcb1dc6460ba2d32c22c257
Francesco Biscani <bluescarni@gmail.com>
2022-03-13 11:07:00 +0100

    Another minor bit.

--- src/math/exp.cpp
+++ src/math/exp.cpp
@@ -111,6 +111,7 @@ long double exp_impl::eval_ldbl(const std::unordered_map<std::string, long doubl
 }
 
 #if defined(HEYOKA_HAVE_REAL128)
+
 mppp::real128 exp_impl::eval_f128(const std::unordered_map<std::string, mppp::real128> &map,
                                   const std::vector<mppp::real128> &pars) const
 {
@@ -118,6 +119,7 @@ mppp::real128 exp_impl::eval_f128(const std::unordered_map<std::string, mppp::re
 
     return mppp::exp(heyoka::eval_f128(args()[0], map, pars));
 }
+
 #endif
 
 void exp_impl::eval_batch_dbl(std::vector<double> &out, const std::unordered_map<std::string, std::vector<double>> &map,
