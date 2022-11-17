7719d5d67dc5bb47441e5d721d19d5d5103a797d
Francesco Biscani <bluescarni@gmail.com>
2022-11-17 10:04:19 +0100

    Taylor mp testing for the hyperbolic and inverse functions.

---
+++ test/taylor_hyper_mp.cpp
@@ -0,0 +1,427 @@
+// Copyright 2020, 2021, 2022 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <initializer_list>
+#include <vector>
+
+#include <mp++/real.hpp>
+
+#include <heyoka/llvm_state.hpp>
+#include <heyoka/math/acosh.hpp>
+#include <heyoka/math/asinh.hpp>
+#include <heyoka/math/atanh.hpp>
+#include <heyoka/math/cosh.hpp>
+#include <heyoka/math/sinh.hpp>
+#include <heyoka/math/sqrt.hpp>
+#include <heyoka/math/tanh.hpp>
+#include <heyoka/taylor.hpp>
+
+#include "catch.hpp"
+#include "test_utils.hpp"
+
+using namespace heyoka;
+using namespace heyoka_test;
+
+TEST_CASE("cosh")
+{
+    using fp_t = mppp::real;
+
+    auto x = "x"_var, y = "y"_var;
+
+    for (auto opt_level : {0u, 3u}) {
+        for (auto cm : {false, true}) {
+            for (auto ha : {false, true}) {
+                for (auto prec : {30, 123}) {
+                    const auto uprec = static_cast<unsigned>(prec);
+
+                    // Test with param.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {cosh(par[0]), x + y}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{3, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(cosh(pars[0])));
+                        REQUIRE(jet[3] == approximately(fp_t{5, prec}));
+                        REQUIRE(jet[4] == 0);
+                        REQUIRE(jet[5] == approximately(fp_t{1, prec} / fp_t{2, prec} * (jet[2] + jet[3])));
+                    }
+                    // Test with variable.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {cosh(y + 2_dbl), par[0] + x}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{-4, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(cosh(jet[1] + fp_t(2, prec))));
+                        REQUIRE(jet[3] == approximately(pars[0] + jet[0]));
+                        REQUIRE(jet[4] == approximately(fp_t(.5, prec) * sinh(jet[1] + fp_t(2, prec)) * jet[3]));
+                        REQUIRE(jet[5] == approximately(fp_t(.5, prec) * jet[2]));
+                    }
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("sinh")
+{
+    using fp_t = mppp::real;
+
+    auto x = "x"_var, y = "y"_var;
+
+    for (auto opt_level : {0u, 3u}) {
+        for (auto cm : {false, true}) {
+            for (auto ha : {false, true}) {
+                for (auto prec : {30, 123}) {
+                    const auto uprec = static_cast<unsigned>(prec);
+
+                    // Test with param.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {sinh(par[0]), x + y}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{3, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(sinh(pars[0])));
+                        REQUIRE(jet[3] == approximately(fp_t{5, prec}));
+                        REQUIRE(jet[4] == 0);
+                        REQUIRE(jet[5] == approximately(fp_t{1, prec} / fp_t{2, prec} * (jet[2] + jet[3])));
+                    }
+                    // Test with variable.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {sinh(y + 2_dbl), par[0] + x}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{-4, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(sinh(jet[1] + fp_t(2, prec))));
+                        REQUIRE(jet[3] == approximately(pars[0] + jet[0]));
+                        REQUIRE(jet[4] == approximately(fp_t(.5, prec) * cosh(jet[1] + fp_t(2, prec)) * jet[3]));
+                        REQUIRE(jet[5] == approximately(fp_t(.5, prec) * jet[2]));
+                    }
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("tanh")
+{
+    using fp_t = mppp::real;
+
+    auto x = "x"_var, y = "y"_var;
+
+    for (auto opt_level : {0u, 3u}) {
+        for (auto cm : {false, true}) {
+            for (auto ha : {false, true}) {
+                for (auto prec : {30, 123}) {
+                    const auto uprec = static_cast<unsigned>(prec);
+
+                    // Test with param.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {tanh(par[0]), x + y}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{3, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(tanh(pars[0])));
+                        REQUIRE(jet[3] == approximately(fp_t{5, prec}));
+                        REQUIRE(jet[4] == 0);
+                        REQUIRE(jet[5] == approximately(fp_t{1, prec} / fp_t{2, prec} * (jet[2] + jet[3])));
+                    }
+                    // Test with variable.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {tanh(y + .2_dbl), par[0] + x}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{-4, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(tanh(jet[1] + fp_t(.2, prec))));
+                        REQUIRE(jet[3] == approximately(pars[0] + jet[0]));
+                        REQUIRE(jet[4]
+                                == approximately(fp_t(.5, prec)
+                                                 / (cosh(jet[1] + fp_t(.2, prec)) * cosh(jet[1] + fp_t(.2, prec)))
+                                                 * jet[3]));
+                        REQUIRE(jet[5] == approximately(fp_t(.5, prec) * jet[2]));
+                    }
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("acosh")
+{
+    using fp_t = mppp::real;
+
+    auto x = "x"_var, y = "y"_var;
+
+    for (auto opt_level : {0u, 3u}) {
+        for (auto cm : {false, true}) {
+            for (auto ha : {false, true}) {
+                for (auto prec : {30, 123}) {
+                    const auto uprec = static_cast<unsigned>(prec);
+
+                    // Test with param.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {acosh(par[0]), x + y}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{1.3, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(acosh(pars[0])));
+                        REQUIRE(jet[3] == approximately(fp_t{5, prec}));
+                        REQUIRE(jet[4] == 0);
+                        REQUIRE(jet[5] == approximately(fp_t{1, prec} / fp_t{2, prec} * (jet[2] + jet[3])));
+                    }
+                    // Test with variable.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {acosh(y + 2_dbl), par[0] + x}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{.3, prec}};
+                        std::vector<fp_t> pars{fp_t{-4, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == fp_t(.3, prec));
+                        REQUIRE(jet[2] == approximately(acosh(jet[1] + fp_t(2, prec))));
+                        REQUIRE(jet[3] == approximately(pars[0] + jet[0]));
+                        REQUIRE(
+                            jet[4]
+                            == approximately(fp_t(.5, prec)
+                                             / sqrt((jet[1] + fp_t(2, prec)) * (jet[1] + fp_t(2, prec)) - fp_t(1, prec))
+                                             * jet[3]));
+                        REQUIRE(jet[5] == approximately(fp_t(.5, prec) * jet[2]));
+                    }
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("asinh")
+{
+    using fp_t = mppp::real;
+
+    auto x = "x"_var, y = "y"_var;
+
+    for (auto opt_level : {0u, 3u}) {
+        for (auto cm : {false, true}) {
+            for (auto ha : {false, true}) {
+                for (auto prec : {30, 123}) {
+                    const auto uprec = static_cast<unsigned>(prec);
+
+                    // Test with param.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {asinh(par[0]), x + y}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{.3, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(asinh(pars[0])));
+                        REQUIRE(jet[3] == approximately(fp_t{5, prec}));
+                        REQUIRE(jet[4] == 0);
+                        REQUIRE(jet[5] == approximately(fp_t{1, prec} / fp_t{2, prec} * (jet[2] + jet[3])));
+                    }
+                    // Test with variable.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {asinh(y + .2_dbl), par[0] + x}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{.3, prec}};
+                        std::vector<fp_t> pars{fp_t{-4, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == fp_t(.3, prec));
+                        REQUIRE(jet[2] == approximately(asinh(jet[1] + fp_t(.2, prec))));
+                        REQUIRE(jet[3] == approximately(pars[0] + jet[0]));
+                        REQUIRE(jet[4]
+                                == approximately(
+                                    fp_t(.5, prec)
+                                    / sqrt(fp_t(1, prec) + (jet[1] + fp_t(.2, prec)) * (jet[1] + fp_t(.2, prec)))
+                                    * jet[3]));
+                        REQUIRE(jet[5] == approximately(fp_t(.5, prec) * jet[2]));
+                    }
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("atanh")
+{
+    using fp_t = mppp::real;
+
+    auto x = "x"_var, y = "y"_var;
+
+    for (auto opt_level : {0u, 3u}) {
+        for (auto cm : {false, true}) {
+            for (auto ha : {false, true}) {
+                for (auto prec : {30, 123}) {
+                    const auto uprec = static_cast<unsigned>(prec);
+
+                    // Test with param.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {atanh(par[0]), x + y}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{.3, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(atanh(pars[0])));
+                        REQUIRE(jet[3] == approximately(fp_t{5, prec}));
+                        REQUIRE(jet[4] == 0);
+                        REQUIRE(jet[5] == approximately(fp_t{1, prec} / fp_t{2, prec} * (jet[2] + jet[3])));
+                    }
+                    // Test with variable.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {atanh(y + .2_dbl), par[0] + x}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{.3, prec}};
+                        std::vector<fp_t> pars{fp_t{-4, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == fp_t(.3, prec));
+                        REQUIRE(jet[2] == approximately(atanh(jet[1] + fp_t(.2, prec))));
+                        REQUIRE(jet[3] == approximately(pars[0] + jet[0]));
+                        REQUIRE(
+                            jet[4]
+                            == approximately(fp_t(.5, prec)
+                                             / (fp_t(1, prec) - (jet[1] + fp_t(.2, prec)) * (jet[1] + fp_t(.2, prec)))
+                                             * jet[3]));
+                        REQUIRE(jet[5] == approximately(fp_t(.5, prec) * jet[2]));
+                    }
+                }
+            }
+        }
+    }
+}
