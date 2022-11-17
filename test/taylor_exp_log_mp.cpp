a31a9f01ea5a16ed754fe0ce223066087d752200
Francesco Biscani <bluescarni@gmail.com>
2022-11-17 10:21:13 +0100

    Forgot about the sigmoid.

--- test/taylor_exp_log_mp.cpp
+++ test/taylor_exp_log_mp.cpp
@@ -14,6 +14,7 @@
 #include <heyoka/llvm_state.hpp>
 #include <heyoka/math/exp.hpp>
 #include <heyoka/math/log.hpp>
+#include <heyoka/math/sigmoid.hpp>
 #include <heyoka/taylor.hpp>
 
 #include "catch.hpp"
@@ -21,6 +22,18 @@
 
 using namespace heyoka;
 using namespace heyoka_test;
+
+namespace
+{
+
+mppp::real sigmoid(mppp::real x)
+{
+    const auto prec = x.get_prec();
+
+    return mppp::real{1., prec} / (mppp::real{1., prec} + mppp::exp(-x));
+}
+
+} // namespace
 
 TEST_CASE("exp")
 {
@@ -142,6 +155,71 @@ TEST_CASE("log")
                         REQUIRE(jet[2] == approximately(log(jet[1] + fp_t(2, prec))));
                         REQUIRE(jet[3] == approximately(pars[0] + jet[0]));
                         REQUIRE(jet[4] == approximately(fp_t(.5, prec) / (jet[1] + fp_t(2, prec)) * jet[3]));
+                        REQUIRE(jet[5] == approximately(fp_t(.5, prec) * jet[2]));
+                    }
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("sigmoid")
+{
+    using fp_t = mppp::real;
+
+    auto x = "x"_var, y = "y"_var;
+
+    for (auto opt_level : {0u, 3u}) {
+        for (auto cm : {false, true}) {
+            for (auto ha : {false, true}) {
+                for (auto prec : {30, 123}) {
+                    const auto uprec = static_cast<unsigned>(prec);
+
+                    // Test with param.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {sigmoid(par[0]), x + y}, 2, 1, ha, cm, {}, false, uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{3, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(sigmoid(pars[0])));
+                        REQUIRE(jet[3] == approximately(fp_t{5, prec}));
+                        REQUIRE(jet[4] == 0);
+                        REQUIRE(jet[5] == approximately(fp_t{1, prec} / fp_t{2, prec} * (jet[2] + jet[3])));
+                    }
+                    // Test with variable.
+                    {
+                        llvm_state s{kw::opt_level = opt_level};
+
+                        taylor_add_jet<fp_t>(s, "jet", {sigmoid(y + 2_dbl), par[0] + x}, 2, 1, ha, cm, {}, false,
+                                             uprec);
+
+                        s.compile();
+
+                        auto jptr = reinterpret_cast<void (*)(fp_t *, const fp_t *, const fp_t *)>(s.jit_lookup("jet"));
+
+                        std::vector<fp_t> jet{fp_t{2, prec}, fp_t{3, prec}};
+                        std::vector<fp_t> pars{fp_t{-4, prec}};
+                        jet.resize(6, fp_t{0, prec});
+
+                        jptr(jet.data(), pars.data(), nullptr);
+
+                        REQUIRE(jet[0] == 2);
+                        REQUIRE(jet[1] == 3);
+                        REQUIRE(jet[2] == approximately(sigmoid(jet[1] + fp_t(2, prec))));
+                        REQUIRE(jet[3] == approximately(pars[0] + jet[0]));
+                        REQUIRE(jet[4] == approximately(fp_t(.5, prec) * jet[2] * (fp_t(1, prec) - jet[2]) * jet[3]));
                         REQUIRE(jet[5] == approximately(fp_t(.5, prec) * jet[2]));
                     }
                 }
