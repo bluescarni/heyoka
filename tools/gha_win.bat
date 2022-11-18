20a9ac5753ed10472650f8cc38178607e6201591
Francesco Biscani <bluescarni@gmail.com>
2022-11-18 11:52:46 +0100

    Try again.

--- tools/gha_win.bat
+++ tools/gha_win.bat
@@ -1,6 +1,6 @@
-conda config --set always_yes yes
-conda create --name heyoka cmake 'llvmdev=15.0.2' tbb-devel tbb boost-cpp xtensor xtensor-blas blas blas-devel fmt spdlog sleef zlib libzlib 'mppp>=0.27'
-conda activate heyoka
+echo "The conda prefix is: "
+echo %CONDA_PREFIX%
+conda install heyoka cmake 'llvmdev=15.0.2' tbb-devel tbb boost-cpp xtensor xtensor-blas blas blas-devel fmt spdlog sleef zlib libzlib 'mppp>=0.27' -y
 mkdir build
 cd build
 cmake ../ -G "Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH=%CONDA_PREFIX_PATH% -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_WITH_MPPP=yes -DHEYOKA_BUILD_TUTORIALS=ON -DHEYOKA_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON -DHEYOKA_WITH_SLEEF=yes
