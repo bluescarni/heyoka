55cc2d38d858079b37d688ca9bd81eec587f6e00
Francesco Biscani <bluescarni@gmail.com>
2022-11-18 13:54:37 +0100

    Another try.

--- tools/gha_win.bat
+++ tools/gha_win.bat
@@ -2,7 +2,7 @@ echo on
 
 echo "The conda prefix is: "
 echo %CONDA_PREFIX%
-conda install ^
+conda install -y ^
     cmake ^
     'llvmdev=15.0.2' ^
     tbb-devel ^
@@ -17,8 +17,7 @@ conda install ^
     sleef ^
     zlib ^
     libzlib ^
-    'mppp>=0.27' ^
-    -y
+    'mppp^>=0.27'
 
 mkdir build
 cd build
