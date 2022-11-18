c4b9acece4449374cd75b98dba15f79dac4dab70
Francesco Biscani <bluescarni@gmail.com>
2022-11-18 13:41:06 +0100

    Try specifying cmd as shell.

--- tools/gha_win.ps1
+++ tools/gha_win.ps1
@@ -1,17 +1,9 @@
 echo "The conda prefix is: "
-echo $env:CONDA_PREFIX
-$env:GMP_INCLUDE_DIR = $env:CONDA_PREFIX
-$env:GMP_INCLUDE_DIR += 'Library\include'
-echo "The GMP include dir is: "
-echo $env:GMP_INCLUDE_DIR
-$env:GMP_LIBRARY = $env:CONDA_PREFIX
-$env:GMP_LIBRARY += 'Library\lib\mpir.lib'
-echo "The GMP library is: "
-echo $env:GMP_LIBRARY
+echo %CONDA_PREFIX%
 conda install heyoka cmake 'llvmdev=15.0.2' tbb-devel tbb boost-cpp xtensor xtensor-blas blas blas-devel fmt spdlog sleef zlib libzlib 'mppp>=0.27' -y
 mkdir build
 cd build
-cmake ../ -G "Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH=%CONDA_PREFIX_PATH% -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_WITH_MPPP=yes -DHEYOKA_BUILD_TUTORIALS=ON -DHEYOKA_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON -DHEYOKA_WITH_SLEEF=yes -DMPPP_GMP_INCLUDE_DIR=$env:GMP_INCLUDE_DIR -DMPPP_GMP_LIBRARY=$env:GMP_LIBRARY
+cmake ../ -G "Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH=%CONDA_PREFIX_PATH% -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_WITH_MPPP=yes -DHEYOKA_BUILD_TUTORIALS=ON -DHEYOKA_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON -DHEYOKA_WITH_SLEEF=yes
 cmake --build . --config Release
 copy Release\heyoka.dll test\Release\
 ctest -j4 -V -C Release
