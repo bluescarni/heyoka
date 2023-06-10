09d7f429e62fa28758ad6e6c3bd11be9b3eb1179
Francesco Biscani <bluescarni@gmail.com>
2023-06-10 16:21:55 +0200

    Initial files for split sub.

---
+++ include/heyoka/detail/sub.hpp
@@ -0,0 +1,61 @@
+// Copyright 2020, 2021, 2022, 2023 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef HEYOKA_DETAIL_SUB_HPP
+#define HEYOKA_DETAIL_SUB_HPP
+
+#include <cstdint>
+#include <vector>
+
+#include <heyoka/config.hpp>
+#include <heyoka/detail/func_cache.hpp>
+#include <heyoka/detail/fwd_decl.hpp>
+#include <heyoka/detail/llvm_fwd.hpp>
+#include <heyoka/detail/visibility.hpp>
+#include <heyoka/func.hpp>
+#include <heyoka/s11n.hpp>
+
+HEYOKA_BEGIN_NAMESPACE
+
+namespace detail
+{
+
+class HEYOKA_DLL_PUBLIC sub_impl : public func_base
+{
+    friend class boost::serialization::access;
+    template <typename Archive>
+    void serialize(Archive &ar, unsigned)
+    {
+        ar &boost::serialization::base_object<func_base>(*this);
+    }
+
+public:
+    sub_impl();
+    explicit sub_impl(expression, expression);
+
+    [[nodiscard]] llvm::Value *llvm_eval(llvm_state &, llvm::Type *, const std::vector<llvm::Value *> &, llvm::Value *,
+                                         llvm::Value *, llvm::Value *, std::uint32_t, bool) const;
+
+    [[nodiscard]] llvm::Function *llvm_c_eval_func(llvm_state &, llvm::Type *, std::uint32_t, bool) const;
+
+    llvm::Value *taylor_diff(llvm_state &, llvm::Type *, const std::vector<std::uint32_t> &,
+                             const std::vector<llvm::Value *> &, llvm::Value *, llvm::Value *, std::uint32_t,
+                             std::uint32_t, std::uint32_t, std::uint32_t, bool) const;
+
+    llvm::Function *taylor_c_diff_func(llvm_state &, llvm::Type *, std::uint32_t, std::uint32_t, bool) const;
+};
+
+HEYOKA_DLL_PUBLIC expression sub(expression, expression);
+
+} // namespace detail
+
+HEYOKA_END_NAMESPACE
+
+HEYOKA_S11N_FUNC_EXPORT_KEY(heyoka::detail::sub_impl)
+
+#endif
