a3b647a9517d190162960f368c57ad503fde7e7e
Francesco Biscani <bluescarni@gmail.com>
2023-08-04 17:01:59 +0200

    Testing for the pre_hook() functionality.

--- test/step_callback.cpp
+++ test/step_callback.cpp
@@ -8,10 +8,15 @@
 
 #include <functional>
 #include <sstream>
+#include <stdexcept>
+#include <tuple>
 #include <type_traits>
 #include <typeinfo>
 #include <utility>
+#include <vector>
 
+#include <heyoka/expression.hpp>
+#include <heyoka/model/pendulum.hpp>
 #include <heyoka/s11n.hpp>
 #include <heyoka/step_callback.hpp>
 #include <heyoka/taylor.hpp>
@@ -43,8 +48,6 @@ struct cb1 {
 
 TEST_CASE("step_callback basics")
 {
-    using Catch::Matchers::Message;
-
     taylor_adaptive<double> ta;
 
     {
@@ -308,5 +311,127 @@ TEST_CASE("step_callback s11n")
         }
 
         REQUIRE(!step_cb);
+    }
+}
+
+struct pend_cb {
+    template <typename TA>
+    bool operator()(TA &)
+    {
+        return true;
+    }
+
+    void pre_hook(taylor_adaptive<double> &ta)
+    {
+        ta.get_pars_data()[0] = 1.5;
+    }
+
+    void pre_hook(taylor_adaptive_batch<double> &ta)
+    {
+        ta.get_pars_data()[0] = 1.5;
+        ta.get_pars_data()[1] = 1.5;
+    }
+};
+
+struct tm_cb {
+    template <typename TA>
+    bool operator()(TA &)
+    {
+        return true;
+    }
+
+    void pre_hook(taylor_adaptive<double> &ta)
+    {
+        ta.set_time(ta.get_time() + 1);
+    }
+
+    void pre_hook(taylor_adaptive_batch<double> &ta)
+    {
+        ta.set_time({ta.get_time()[0] + 1, ta.get_time()[1] + 1});
+    }
+};
+
+TEST_CASE("step_callback pre_hook")
+{
+    using Catch::Matchers::Message;
+
+    auto dyn = model::pendulum(kw::l = par[0]);
+
+    {
+        auto ta0 = taylor_adaptive<double>{dyn, {1., 0.}};
+        auto ta1 = taylor_adaptive<double>{dyn, {1., 0.}, kw::pars = {1.5}};
+
+        REQUIRE(ta0.get_pars()[0] == 0.);
+
+        ta0.propagate_until(3., kw::callback = pend_cb{});
+        ta1.propagate_until(3.);
+
+        REQUIRE(ta0.get_pars()[0] == 1.5);
+
+        REQUIRE(ta0.get_state() == ta1.get_state());
+
+        REQUIRE_THROWS_MATCHES(
+            ta0.propagate_until(6., kw::callback = tm_cb{}), std::runtime_error,
+            Message("The invocation of the callback passed to propagate_until() resulted in the alteration of the "
+                    "time coordinate of the integrator - this is not supported"));
+
+        REQUIRE(ta0.get_time() == 4);
+
+        ta0.set_time(0.);
+        ta0.get_pars_data()[0] = 0.1;
+        ta1.set_time(0.);
+
+        auto res0 = ta0.propagate_grid({0., 1., 2.}, kw::callback = pend_cb{});
+        auto res1 = ta1.propagate_grid({0., 1., 2.});
+
+        REQUIRE(std::get<4>(res0)[0] == std::get<4>(res1)[0]);
+
+        REQUIRE(ta0.get_pars()[0] == 1.5);
+
+        REQUIRE_THROWS_MATCHES(
+            ta0.propagate_grid({3., 4.}, kw::callback = tm_cb{}), std::runtime_error,
+            Message("The invocation of the callback passed to propagate_grid() resulted in the alteration of the "
+                    "time coordinate of the integrator - this is not supported"));
+    }
+
+    {
+        auto ta0 = taylor_adaptive_batch<double>{dyn, {1., 1.1, 0., 0.1}, 2u};
+        auto ta1 = taylor_adaptive_batch<double>{dyn, {1., 1.1, 0., 0.1}, 2u, kw::pars = {1.5, 1.5}};
+
+        REQUIRE(ta0.get_pars()[0] == 0.);
+        REQUIRE(ta0.get_pars()[1] == 0.);
+
+        ta0.propagate_until(3., kw::callback = pend_cb{});
+        ta1.propagate_until(3.);
+
+        REQUIRE(ta0.get_pars()[0] == 1.5);
+        REQUIRE(ta0.get_pars()[1] == 1.5);
+
+        REQUIRE(ta0.get_state() == ta1.get_state());
+
+        REQUIRE_THROWS_MATCHES(
+            ta0.propagate_until(6., kw::callback = tm_cb{}), std::runtime_error,
+            Message("The invocation of the callback passed to propagate_until() resulted in the alteration of the "
+                    "time coordinate of the integrator - this is not supported"));
+
+        REQUIRE(ta0.get_time() == std::vector{4., 4.});
+
+        ta0.set_time(0.);
+        ta0.get_pars_data()[0] = 0.1;
+        ta0.get_pars_data()[1] = 0.1;
+        ta1.set_time(0.);
+
+        auto res0 = ta0.propagate_grid({0., 0., 1., 1., 2., 2.}, kw::callback = pend_cb{});
+        auto res1 = ta1.propagate_grid({0., 0., 1., 1., 2., 2.});
+
+        REQUIRE(res0 == res1);
+
+        REQUIRE(ta0.get_pars()[0] == 1.5);
+        REQUIRE(ta0.get_pars()[1] == 1.5);
+
+        REQUIRE_THROWS_MATCHES(
+            ta0.propagate_grid({3., 3., 4., 4.}, kw::callback = tm_cb{}), std::runtime_error,
+            Message("The invocation of the callback passed to propagate_grid() resulted in the alteration of the "
+                    "time coordinate of the integrator - this is not supported"));
     }
 }
