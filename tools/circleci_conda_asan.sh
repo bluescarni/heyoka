00a04f10fdd85647e697797f7e6045932c186a3f
Francesco Biscani <bluescarni@gmail.com>
2023-10-04 14:25:20 +0200

    Revert "Increase verbosity."
    This reverts commit be8eeb2cc7896df3039db54cfd232524ea929cfa.

--- tools/circleci_conda_asan.sh
+++ tools/circleci_conda_asan.sh
@@ -16,7 +16,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge
 conda config --set channel_priority strict
-conda create -y -p $deps_dir c-compiler cxx-compiler cmake \
+conda create -y -q -p $deps_dir c-compiler cxx-compiler cmake \
     llvmdev tbb-devel tbb boost-cpp 'mppp>=0.27' sleef xtensor \
     xtensor-blas blas blas-devel fmt spdlog sphinx \
     'sphinxcontrib-bibtex<2.6' sphinx-book-theme
