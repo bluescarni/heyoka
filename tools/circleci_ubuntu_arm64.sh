a53f34c7513b391b6a4a0ca8ec3eee4eafb6aace
Francesco Biscani <bluescarni@gmail.com>
2023-12-10 21:49:38 +0100

    CI updates.

--- tools/circleci_ubuntu_arm64.sh
+++ tools/circleci_ubuntu_arm64.sh
@@ -14,7 +14,7 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforg
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-mamba create -y -p $deps_dir cxx-compiler c-compiler cmake llvmdev tbb-devel tbb boost-cpp sleef xtensor xtensor-blas blas blas-devel fmt spdlog 'mppp>=0.27'
+mamba create -y -p $deps_dir cxx-compiler c-compiler cmake llvmdev tbb-devel tbb boost-cpp sleef xtensor xtensor-blas blas blas-devel fmt spdlog 'mppp=1.*'
 source activate $deps_dir
 
 # Create the build dir and cd into it.
