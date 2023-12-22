d7be6aa4c403cc853eb44ca8a09f9b525eeb3a53
Francesco Biscani <bluescarni@gmail.com>
2023-12-22 11:13:09 +0100

    Initial version of the angle reducer, yet to be tested.

---
+++ include/heyoka/callback/angle_reducer.hpp
@@ -0,0 +1,152 @@
+// Copyright 2020, 2021, 2022, 2023 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef HEYOKA_CALLBACK_ANGLE_REDUCER_HPP
+#define HEYOKA_CALLBACK_ANGLE_REDUCER_HPP
+
+#include <heyoka/config.hpp>
+
+#include <concepts>
+#include <iterator>
+#include <memory>
+#include <ranges>
+#include <type_traits>
+#include <unordered_set>
+#include <utility>
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+#include <mp++/real128.hpp>
+
+#endif
+
+#if defined(HEYOKA_HAVE_REAL)
+
+#include <mp++/real.hpp>
+
+#endif
+
+#include <heyoka/detail/fwd_decl.hpp>
+#include <heyoka/detail/visibility.hpp>
+#include <heyoka/expression.hpp>
+#include <heyoka/s11n.hpp>
+#include <heyoka/step_callback.hpp>
+
+HEYOKA_BEGIN_NAMESPACE
+
+namespace callback
+{
+
+class HEYOKA_DLL_PUBLIC angle_reducer
+{
+    class impl;
+
+    std::unique_ptr<impl> m_impl;
+
+    void validate_and_construct(std::unordered_set<expression>);
+
+    template <typename TA>
+    HEYOKA_DLL_LOCAL void pre_hook_impl(TA &);
+
+    // Serialisation.
+    friend class boost::serialization::access;
+    template <typename Archive>
+    void serialize(Archive &ar, unsigned)
+    {
+        ar & m_impl;
+    }
+
+public:
+    angle_reducer();
+    angle_reducer(const angle_reducer &);
+    angle_reducer(angle_reducer &&) noexcept;
+    angle_reducer &operator=(const angle_reducer &);
+    angle_reducer &operator=(angle_reducer &&) noexcept;
+    ~angle_reducer();
+
+    template <typename R>
+        requires(!std::same_as<angle_reducer, std::remove_cvref_t<R>>) && std::ranges::input_range<R>
+                && std::constructible_from<expression, std::iter_reference_t<std::ranges::iterator_t<R>>>
+    explicit angle_reducer(R &&r)
+    {
+        // Turn into lvalue.
+        auto &&rl = std::forward<R>(r);
+
+        std::unordered_set<expression> var_set;
+        for (auto &&x : rl) {
+            var_set.emplace(std::forward<decltype(x)>(x));
+        }
+
+        validate_and_construct(std::move(var_set));
+    }
+
+    template <typename T>
+    void pre_hook(taylor_adaptive<T> &);
+    template <typename T>
+    bool operator()(taylor_adaptive<T> &);
+
+    template <typename T>
+    void pre_hook(taylor_adaptive_batch<T> &);
+    template <typename T>
+    bool operator()(taylor_adaptive_batch<T> &);
+};
+
+// Prevent implicit instantiations of the member functions.
+#define HEYOKA_CALLBACK_ANGLE_REDUCER_EXTERN_INST(F)                                                                   \
+    extern template bool angle_reducer::operator()<F>(taylor_adaptive<F> &);                                           \
+    extern template void angle_reducer::pre_hook<F>(taylor_adaptive<F> &);                                             \
+    extern template bool angle_reducer::operator()<F>(taylor_adaptive_batch<F> &);                                     \
+    extern template void angle_reducer::pre_hook<F>(taylor_adaptive_batch<F> &);
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_EXTERN_INST(float)
+HEYOKA_CALLBACK_ANGLE_REDUCER_EXTERN_INST(double)
+HEYOKA_CALLBACK_ANGLE_REDUCER_EXTERN_INST(long double)
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_EXTERN_INST(mppp::real128)
+
+#endif
+
+#if defined(HEYOKA_HAVE_REAL)
+
+// NOTE: prevent the instantiation for the batch version of mppp::real as well.
+HEYOKA_CALLBACK_ANGLE_REDUCER_EXTERN_INST(mppp::real)
+
+#endif
+
+#undef HEYOKA_CALLBACK_ANGLE_REDUCER_EXTERN_INST
+
+} // namespace callback
+
+HEYOKA_END_NAMESPACE
+
+// Serialisation macros for angle_reducer.
+#define HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_KEY(F)                                                               \
+    HEYOKA_S11N_STEP_CALLBACK_EXPORT_KEY(heyoka::callback::angle_reducer, F)                                           \
+    HEYOKA_S11N_STEP_CALLBACK_BATCH_EXPORT_KEY(heyoka::callback::angle_reducer, F)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_KEY(float)
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_KEY(double)
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_KEY(long double)
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_KEY(mppp::real128)
+
+#endif
+
+#if defined(HEYOKA_HAVE_REAL)
+
+HEYOKA_S11N_STEP_CALLBACK_EXPORT_KEY(heyoka::callback::angle_reducer, mppp::real)
+
+#endif
+
+#undef HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_KEY
+
+#endif
---
+++ src/callback/angle_reducer.cpp
@@ -0,0 +1,338 @@
+// Copyright 2020, 2021, 2022, 2023 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <heyoka/config.hpp>
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <stdexcept>
+#include <unordered_set>
+#include <utility>
+#include <variant>
+#include <vector>
+
+#include <boost/math/constants/constants.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/serialization/unordered_set.hpp>
+
+#include <fmt/core.h>
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+#include <mp++/real128.hpp>
+
+#endif
+
+#if defined(HEYOKA_HAVE_REAL)
+
+#include <mp++/real.hpp>
+
+#endif
+
+#include <heyoka/callback/angle_reducer.hpp>
+#include <heyoka/detail/visibility.hpp>
+#include <heyoka/expression.hpp>
+#include <heyoka/s11n.hpp>
+#include <heyoka/step_callback.hpp>
+#include <heyoka/taylor.hpp>
+#include <heyoka/variable.hpp>
+
+HEYOKA_BEGIN_NAMESPACE
+
+namespace callback
+{
+
+class angle_reducer::impl
+{
+public:
+    std::unordered_set<expression> m_var_set;
+    std::vector<std::size_t> m_ind;
+
+private:
+    // Serialisation.
+    friend class boost::serialization::access;
+    template <typename Archive>
+    void serialize(Archive &ar, unsigned)
+    {
+        ar & m_var_set;
+        ar & m_ind;
+    }
+
+    impl() = default;
+
+public:
+    explicit impl(std::unordered_set<expression> var_set) : m_var_set(std::move(var_set)) {}
+};
+
+angle_reducer::angle_reducer() = default;
+
+angle_reducer::angle_reducer(const angle_reducer &other)
+    : m_impl(other.m_impl ? std::make_unique<impl>(*other.m_impl) : nullptr)
+{
+}
+
+angle_reducer::angle_reducer(angle_reducer &&) noexcept = default;
+
+angle_reducer &angle_reducer::operator=(const angle_reducer &other)
+{
+    if (this != &other) {
+        *this = angle_reducer(other);
+    }
+
+    return *this;
+}
+
+angle_reducer &angle_reducer::operator=(angle_reducer &&) noexcept = default;
+
+angle_reducer::~angle_reducer() = default;
+
+void angle_reducer::validate_and_construct(std::unordered_set<expression> var_set)
+{
+    if (var_set.empty()) {
+        throw std::invalid_argument(
+            "The list of expressions passed to the constructor of angle_reducer cannot be empty");
+    }
+
+    if (std::ranges::any_of(var_set, [](const auto &ex) { return !std::holds_alternative<variable>(ex.value()); })) {
+        throw std::invalid_argument(
+            "The list of expressions passed to the constructor of angle_reducer can contain only variables");
+    }
+
+    m_impl = std::make_unique<impl>(std::move(var_set));
+}
+
+namespace detail
+{
+
+namespace
+{
+
+constexpr auto invalid_ar_msg = "Cannot use an angle_reducer which was default-constructed or moved-from";
+
+} // namespace
+
+} // namespace detail
+
+template <typename TA>
+void angle_reducer::pre_hook_impl(TA &ta)
+{
+    if (!m_impl) {
+        throw std::invalid_argument(detail::invalid_ar_msg);
+    }
+
+    // Reset and rebuild the vector of indices.
+    auto &ind_v = m_impl->m_ind;
+    const auto &var_set = m_impl->m_var_set;
+
+    ind_v.clear();
+    for (decltype(ta.get_state_vars().size()) i = 0; i < ta.get_state_vars().size(); ++i) {
+        const auto &ex = ta.get_state_vars()[i];
+
+        assert(std::holds_alternative<variable>(ex.value()));
+
+        if (var_set.contains(ex)) {
+            ind_v.push_back(boost::numeric_cast<std::size_t>(i));
+        }
+    }
+}
+
+template <typename T>
+void angle_reducer::pre_hook(taylor_adaptive<T> &ta)
+{
+    pre_hook_impl(ta);
+}
+
+template <typename T>
+void angle_reducer::pre_hook(taylor_adaptive_batch<T> &ta)
+{
+    pre_hook_impl(ta);
+}
+
+namespace detail
+{
+
+namespace
+{
+
+// Helpers to fetch the constant 2*pi.
+template <typename T>
+T get_twopi_const(const taylor_adaptive<T> &)
+{
+    return 2 * boost::math::constants::pi<T>();
+}
+
+template <typename T>
+T get_twopi_const(const taylor_adaptive_batch<T> &)
+{
+    return 2 * boost::math::constants::pi<T>();
+}
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+template <>
+mppp::real128 get_twopi_const(const taylor_adaptive<mppp::real128> &)
+{
+    return 2 * mppp::pi_128;
+}
+
+template <>
+mppp::real128 get_twopi_const(const taylor_adaptive_batch<mppp::real128> &)
+{
+    return 2 * mppp::pi_128;
+}
+
+#endif
+
+#if defined(HEYOKA_HAVE_REAL)
+
+template <>
+mppp::real get_twopi_const(const taylor_adaptive<mppp::real> &ta)
+{
+    auto ret = mppp::real_pi(ta.get_prec());
+    mul_2ui(ret, ret, 2u);
+
+    return ret;
+}
+
+#endif
+
+} // namespace
+
+} // namespace detail
+
+template <typename T>
+bool angle_reducer::operator()(taylor_adaptive<T> &ta)
+{
+    if (!m_impl) {
+        throw std::invalid_argument(detail::invalid_ar_msg);
+    }
+
+    // Validate m_ind against the integrator object.
+    const auto &ind_v = m_impl->m_ind;
+    assert(std::ranges::is_sorted(ind_v));
+    if (!ind_v.empty() && ind_v.back() >= ta.get_state().size()) {
+        throw std::invalid_argument(
+            fmt::format("Inconsistent state detected in angle_reducer: the last index in the indices vector has a "
+                        "value of {}, but the size of the state vector is only {}",
+                        ind_v.back(), ta.get_state().size()));
+    }
+
+    // Fetch the 2pi constant.
+    const auto twopi_const = detail::get_twopi_const(ta);
+
+    // Run the reduction.
+    auto *sptr = ta.get_state_data();
+    for (const auto ind : ind_v) {
+        using std::floor;
+        sptr[ind] -= twopi_const * floor(sptr[ind] / twopi_const);
+    }
+
+    return true;
+}
+
+template <typename T>
+bool angle_reducer::operator()(taylor_adaptive_batch<T> &ta)
+{
+    if (!m_impl) {
+        throw std::invalid_argument(detail::invalid_ar_msg);
+    }
+
+    // Fetch the batch size.
+    const auto batch_size = ta.get_batch_size();
+    assert(batch_size != 0u);
+
+    // Validate m_ind against the integrator object.
+    const auto &ind_v = m_impl->m_ind;
+    assert(std::ranges::is_sorted(ind_v));
+    if (!ind_v.empty() && ind_v.back() >= ta.get_state().size() / batch_size) {
+        throw std::invalid_argument(
+            fmt::format("Inconsistent state detected in angle_reducer: the last index in the indices vector has a "
+                        "value of {}, but the size of the state vector is only {}",
+                        ind_v.back(), ta.get_state().size() / batch_size));
+    }
+
+    // Fetch the 2pi constant.
+    const auto twopi_const = detail::get_twopi_const(ta);
+
+    // Run the reduction.
+    auto *sptr = ta.get_state_data();
+    for (const auto ind : ind_v) {
+        const auto base_idx = ind * batch_size;
+
+        for (std::uint32_t i = 0; i < batch_size; ++i) {
+            using std::floor;
+            sptr[base_idx + i] -= twopi_const * floor(sptr[base_idx + i] / twopi_const);
+        }
+    }
+
+    return true;
+}
+
+// Explicit insantiations.
+#define HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_SCALAR(F)                                                          \
+    template HEYOKA_DLL_PUBLIC void angle_reducer::pre_hook(taylor_adaptive<F> &);                                     \
+    template HEYOKA_DLL_PUBLIC bool angle_reducer::operator()(taylor_adaptive<F> &);
+
+#define HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_BATCH(F)                                                           \
+    template HEYOKA_DLL_PUBLIC void angle_reducer::pre_hook(taylor_adaptive_batch<F> &);                               \
+    template HEYOKA_DLL_PUBLIC bool angle_reducer::operator()(taylor_adaptive_batch<F> &);
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_SCALAR(float)
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_SCALAR(double)
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_SCALAR(long double)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_BATCH(float)
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_BATCH(double)
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_BATCH(long double)
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_SCALAR(mppp::real128)
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_BATCH(mppp::real128)
+
+#endif
+
+#if defined(HEYOKA_HAVE_REAL)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_SCALAR(mppp::real)
+
+#endif
+
+#undef HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_SCALAR
+#undef HEYOKA_CALLBACK_ANGLE_REDUCER_TEMPLATE_INST_BATCH
+
+} // namespace callback
+
+HEYOKA_END_NAMESPACE
+
+// Serialisation support.
+#define HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_IMPLEMENT(F)                                                         \
+    HEYOKA_S11N_STEP_CALLBACK_EXPORT_IMPLEMENT(heyoka::callback::angle_reducer, F)                                     \
+    HEYOKA_S11N_STEP_CALLBACK_BATCH_EXPORT_IMPLEMENT(heyoka::callback::angle_reducer, F)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_IMPLEMENT(float)
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_IMPLEMENT(double)
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_IMPLEMENT(long double)
+
+#if defined(HEYOKA_HAVE_REAL128)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_IMPLEMENT(mppp::real128)
+
+#endif
+
+#if defined(HEYOKA_HAVE_REAL)
+
+HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_IMPLEMENT(mppp::real)
+
+#endif
+
+#undef HEYOKA_CALLBACK_ANGLE_REDUCER_S11N_EXPORT_IMPLEMENT
