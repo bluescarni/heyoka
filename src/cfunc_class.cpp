b1ce872c20fc7e4dd4877feb692dad9114d719ce
Francesco Biscani <bluescarni@gmail.com>
2024-02-02 08:50:27 +0100

    Fix shadowing mess.

--- src/cfunc_class.cpp
+++ src/cfunc_class.cpp
@@ -52,65 +52,65 @@ struct cfunc<T>::impl {
     using cfunc_ptr_s_t = void (*)(T *, const T *, const T *, const T *, std::size_t) noexcept;
 
     // Data members.
-    std::vector<expression> fn;
-    std::vector<expression> vars;
-    llvm_state s_scal;
-    llvm_state s_batch;
-    llvm_state s_scal_s;
-    llvm_state s_batch_s;
-    std::uint32_t simd_size = 0;
-    std::vector<expression> dc;
-    cfunc_ptr_t fptr_scal = nullptr;
-    cfunc_ptr_t fptr_batch = nullptr;
-    cfunc_ptr_s_t fptr_scal_s = nullptr;
-    cfunc_ptr_s_t fptr_batch_s = nullptr;
-    std::uint32_t nparams = 0;
-    bool is_time_dependent = false;
-    std::uint32_t nouts = 0;
-    std::uint32_t nvars = 0;
-    long long prec = 0;
-    bool check_prec = false;
+    std::vector<expression> m_fn;
+    std::vector<expression> m_vars;
+    llvm_state m_s_scal;
+    llvm_state m_s_batch;
+    llvm_state m_s_scal_s;
+    llvm_state m_s_batch_s;
+    std::uint32_t m_simd_size = 0;
+    std::vector<expression> m_dc;
+    cfunc_ptr_t m_fptr_scal = nullptr;
+    cfunc_ptr_t m_fptr_batch = nullptr;
+    cfunc_ptr_s_t m_fptr_scal_s = nullptr;
+    cfunc_ptr_s_t m_fptr_batch_s = nullptr;
+    std::uint32_t m_nparams = 0;
+    bool m_is_time_dependent = false;
+    std::uint32_t m_nouts = 0;
+    std::uint32_t m_nvars = 0;
+    long long m_prec = 0;
+    bool m_check_prec = false;
 
     // Serialization.
     void save(boost::archive::binary_oarchive &ar, unsigned) const
     {
-        ar << fn;
-        ar << vars;
-        ar << s_scal;
-        ar << s_batch;
-        ar << s_scal_s;
-        ar << s_batch_s;
-        ar << simd_size;
-        ar << dc;
-        ar << nparams;
-        ar << is_time_dependent;
-        ar << nouts;
-        ar << nvars;
-        ar << prec;
-        ar << check_prec;
+        ar << m_fn;
+        ar << m_vars;
+        ar << m_s_scal;
+        ar << m_s_batch;
+        ar << m_s_scal_s;
+        ar << m_s_batch_s;
+        ar << m_simd_size;
+        ar << m_dc;
+        ar << m_nparams;
+        ar << m_is_time_dependent;
+        ar << m_nouts;
+        ar << m_nvars;
+        ar << m_prec;
+        ar << m_check_prec;
     }
     void load(boost::archive::binary_iarchive &ar, unsigned)
     {
-        ar >> fn;
-        ar >> vars;
-        ar >> s_scal;
-        ar >> s_batch;
-        ar >> s_scal_s;
-        ar >> s_batch_s;
-        ar >> simd_size;
-        ar >> dc;
-        ar >> nparams;
-        ar >> is_time_dependent;
-        ar >> nouts;
-        ar >> nvars;
-        ar >> prec;
-        ar >> check_prec;
+        ar >> m_fn;
+        ar >> m_vars;
+        ar >> m_s_scal;
+        ar >> m_s_batch;
+        ar >> m_s_scal_s;
+        ar >> m_s_batch_s;
+        ar >> m_simd_size;
+        ar >> m_dc;
+        ar >> m_nparams;
+        ar >> m_is_time_dependent;
+        ar >> m_nouts;
+        ar >> m_nvars;
+        ar >> m_prec;
+        ar >> m_check_prec;
 
         // Recover the function pointers.
-        fptr_scal = reinterpret_cast<cfunc_ptr_t>(s_scal.jit_lookup("cfunc"));
-        fptr_batch = reinterpret_cast<cfunc_ptr_t>(s_batch.jit_lookup("cfunc"));
-        fptr_scal_s = reinterpret_cast<cfunc_ptr_s_t>(s_scal_s.jit_lookup("cfunc.strided"));
-        fptr_batch_s = reinterpret_cast<cfunc_ptr_s_t>(s_batch_s.jit_lookup("cfunc.strided"));
+        m_fptr_scal = reinterpret_cast<cfunc_ptr_t>(m_s_scal.jit_lookup("cfunc"));
+        m_fptr_batch = reinterpret_cast<cfunc_ptr_t>(m_s_batch.jit_lookup("cfunc"));
+        m_fptr_scal_s = reinterpret_cast<cfunc_ptr_s_t>(m_s_scal_s.jit_lookup("cfunc.strided"));
+        m_fptr_batch_s = reinterpret_cast<cfunc_ptr_s_t>(m_s_batch_s.jit_lookup("cfunc.strided"));
     }
     BOOST_SERIALIZATION_SPLIT_MEMBER()
 
@@ -123,17 +123,17 @@ struct cfunc<T>::impl {
     explicit impl(std::vector<expression> fn, std::vector<expression> vars, llvm_state s,
                   std::optional<std::uint32_t> batch_size, bool high_accuracy, bool compact_mode, bool parallel_mode,
                   long long prec, bool check_prec)
-        : fn(std::move(fn)), vars(std::move(vars)), s_scal(std::move(s)), s_batch(s_scal), s_scal_s(s_scal),
-          s_batch_s(s_scal), prec(prec), check_prec(check_prec)
+        : m_fn(std::move(fn)), m_vars(std::move(vars)), m_s_scal(std::move(s)), m_s_batch(m_s_scal),
+          m_s_scal_s(m_s_scal), m_s_batch_s(m_s_scal), m_prec(prec), m_check_prec(check_prec)
     {
         // Compute the SIMD size.
         // NOLINTNEXTLINE(cppcoreguidelines-prefer-member-initializer)
-        simd_size = batch_size ? *batch_size : recommended_simd_size<T>();
+        m_simd_size = batch_size ? *batch_size : recommended_simd_size<T>();
 
 #if defined(HEYOKA_HAVE_REAL)
 
         // NOTE: batch size > 1u not supported for real.
-        if (std::same_as<T, mppp::real> && simd_size > 1u) {
+        if (std::same_as<T, mppp::real> && m_simd_size > 1u) {
             throw std::invalid_argument("Batch size > 1 is not supported for mppp::real");
         }
 
@@ -145,65 +145,67 @@ struct cfunc<T>::impl {
                 // Scalar unstrided.
                 // NOTE: we fetch the decomposition from the scalar
                 // unstrided invocation of add_cfunc().
-                dc = add_cfunc<T>(s_scal, "cfunc", fn, vars, kw::high_accuracy = high_accuracy,
-                                  kw::compact_mode = compact_mode, kw::parallel_mode = parallel_mode, kw::prec = prec);
+                m_dc = add_cfunc<T>(m_s_scal, "cfunc", m_fn, m_vars, kw::high_accuracy = high_accuracy,
+                                    kw::compact_mode = compact_mode, kw::parallel_mode = parallel_mode,
+                                    kw::prec = m_prec);
 
-                s_scal.compile();
+                m_s_scal.compile();
 
-                fptr_scal = reinterpret_cast<cfunc_ptr_t>(s_scal.jit_lookup("cfunc"));
+                m_fptr_scal = reinterpret_cast<cfunc_ptr_t>(m_s_scal.jit_lookup("cfunc"));
             },
             [&]() {
                 // Batch unstrided.
-                add_cfunc<T>(s_batch, "cfunc", fn, vars, kw::batch_size = simd_size, kw::high_accuracy = high_accuracy,
-                             kw::compact_mode = compact_mode, kw::parallel_mode = parallel_mode, kw::prec = prec);
+                add_cfunc<T>(m_s_batch, "cfunc", m_fn, m_vars, kw::batch_size = m_simd_size,
+                             kw::high_accuracy = high_accuracy, kw::compact_mode = compact_mode,
+                             kw::parallel_mode = parallel_mode, kw::prec = m_prec);
 
-                s_batch.compile();
+                m_s_batch.compile();
 
-                fptr_batch = reinterpret_cast<cfunc_ptr_t>(s_batch.jit_lookup("cfunc"));
+                m_fptr_batch = reinterpret_cast<cfunc_ptr_t>(m_s_batch.jit_lookup("cfunc"));
             },
             [&]() {
                 // Scalar strided.
-                add_cfunc<T>(s_scal_s, "cfunc.strided", fn, vars, kw::high_accuracy = high_accuracy,
-                             kw::compact_mode = compact_mode, kw::parallel_mode = parallel_mode, kw::prec = prec,
+                add_cfunc<T>(m_s_scal_s, "cfunc.strided", m_fn, m_vars, kw::high_accuracy = high_accuracy,
+                             kw::compact_mode = compact_mode, kw::parallel_mode = parallel_mode, kw::prec = m_prec,
                              kw::strided = true);
 
-                s_scal_s.compile();
+                m_s_scal_s.compile();
 
-                fptr_scal_s = reinterpret_cast<cfunc_ptr_s_t>(s_scal_s.jit_lookup("cfunc.strided"));
+                m_fptr_scal_s = reinterpret_cast<cfunc_ptr_s_t>(m_s_scal_s.jit_lookup("cfunc.strided"));
             },
             [&]() {
                 // Batch strided.
-                add_cfunc<T>(s_batch_s, "cfunc.strided", fn, vars, kw::batch_size = simd_size,
+                add_cfunc<T>(m_s_batch_s, "cfunc.strided", m_fn, m_vars, kw::batch_size = m_simd_size,
                              kw::high_accuracy = high_accuracy, kw::compact_mode = compact_mode,
-                             kw::parallel_mode = parallel_mode, kw::prec = prec, kw::strided = true);
+                             kw::parallel_mode = parallel_mode, kw::prec = m_prec, kw::strided = true);
 
-                s_batch_s.compile();
+                m_s_batch_s.compile();
 
-                fptr_batch_s = reinterpret_cast<cfunc_ptr_s_t>(s_batch_s.jit_lookup("cfunc.strided"));
+                m_fptr_batch_s = reinterpret_cast<cfunc_ptr_s_t>(m_s_batch_s.jit_lookup("cfunc.strided"));
             });
 
         // Let's figure out if fn contains params and if it is time-dependent.
-        nparams = get_param_size(fn);
-        is_time_dependent = heyoka::is_time_dependent(fn);
+        m_nparams = get_param_size(m_fn);
+        m_is_time_dependent = heyoka::is_time_dependent(m_fn);
 
         // Cache the number of variables and outputs.
         // NOTE: static casts should also be fine here, because add_cfunc()
         // succeeded and that guarantees that the number of vars and outputs
         // fits in a 32-bit int.
-        nouts = boost::numeric_cast<std::uint32_t>(fn.size());
-        nvars = boost::numeric_cast<std::uint32_t>(vars.size());
+        m_nouts = boost::numeric_cast<std::uint32_t>(m_fn.size());
+        m_nvars = boost::numeric_cast<std::uint32_t>(m_vars.size());
     }
     impl(const impl &other)
-        : fn(other.fn), vars(other.vars), s_scal(other.s_scal), s_batch(other.s_batch), s_scal_s(other.s_scal_s),
-          s_batch_s(other.s_batch_s), simd_size(other.simd_size), dc(other.dc), nparams(other.nparams),
-          is_time_dependent(other.is_time_dependent), nouts(other.nouts), nvars(other.nvars), prec(other.prec),
-          check_prec(other.check_prec)
+        : m_fn(other.m_fn), m_vars(other.m_vars), m_s_scal(other.m_s_scal), m_s_batch(other.m_s_batch),
+          m_s_scal_s(other.m_s_scal_s), m_s_batch_s(other.m_s_batch_s), m_simd_size(other.m_simd_size),
+          m_dc(other.m_dc), m_nparams(other.m_nparams), m_is_time_dependent(other.m_is_time_dependent),
+          m_nouts(other.m_nouts), m_nvars(other.m_nvars), m_prec(other.m_prec), m_check_prec(other.m_check_prec)
     {
         // Recover the function pointers.
-        fptr_scal = reinterpret_cast<cfunc_ptr_t>(s_scal.jit_lookup("cfunc"));
-        fptr_batch = reinterpret_cast<cfunc_ptr_t>(s_batch.jit_lookup("cfunc"));
-        fptr_scal_s = reinterpret_cast<cfunc_ptr_s_t>(s_scal_s.jit_lookup("cfunc.strided"));
-        fptr_batch_s = reinterpret_cast<cfunc_ptr_s_t>(s_batch_s.jit_lookup("cfunc.strided"));
+        m_fptr_scal = reinterpret_cast<cfunc_ptr_t>(m_s_scal.jit_lookup("cfunc"));
+        m_fptr_batch = reinterpret_cast<cfunc_ptr_t>(m_s_batch.jit_lookup("cfunc"));
+        m_fptr_scal_s = reinterpret_cast<cfunc_ptr_s_t>(m_s_scal_s.jit_lookup("cfunc.strided"));
+        m_fptr_batch_s = reinterpret_cast<cfunc_ptr_s_t>(m_s_batch_s.jit_lookup("cfunc.strided"));
     }
 
     // These are never needed.
@@ -233,7 +235,10 @@ cfunc<T>::cfunc(std::vector<expression> fn, std::vector<expression> vars,
 }
 
 template <typename T>
-cfunc<T>::cfunc(const cfunc &other) : m_impl(other.m_impl ? std::make_unique<impl>(*other.m_impl) : nullptr)
+cfunc<T>::cfunc(const cfunc &other)
+    : m_impl(
+        // NOTE: support copy-construction from invalid object.
+        other.m_impl ? std::make_unique<impl>(*other.m_impl) : nullptr)
 {
 }
 
@@ -291,44 +296,44 @@ void cfunc<T>::operator()(out_1d outputs, in_1d inputs, std::optional<in_1d> par
     check_valid(__func__);
 
     // Check the arguments.
-    if (outputs.size() != m_impl->nouts) [[unlikely]] {
+    if (outputs.size() != m_impl->m_nouts) [[unlikely]] {
         throw std::invalid_argument(fmt::format("Invalid outputs array passed to a cfunc: the number of function "
                                                 "outputs is {}, but the outputs array has a size of {}",
-                                                m_impl->nouts, outputs.size()));
+                                                m_impl->m_nouts, outputs.size()));
     }
 
-    if (inputs.size() != m_impl->nvars) [[unlikely]] {
+    if (inputs.size() != m_impl->m_nvars) [[unlikely]] {
         throw std::invalid_argument(fmt::format("Invalid inputs array passed to a cfunc: the number of function "
                                                 "inputs is {}, but the inputs array has a size of {}",
-                                                m_impl->nvars, inputs.size()));
+                                                m_impl->m_nvars, inputs.size()));
     }
 
-    if (m_impl->nparams != 0u && !pars) [[unlikely]] {
+    if (m_impl->m_nparams != 0u && !pars) [[unlikely]] {
         throw std::invalid_argument(
             "An array of parameter values must be passed in order to evaluate a function with parameters");
     }
 
-    if (pars && pars->size() != m_impl->nparams) [[unlikely]] {
+    if (pars && pars->size() != m_impl->m_nparams) [[unlikely]] {
         throw std::invalid_argument(fmt::format("The array of parameter values provided for the evaluation "
                                                 "of a compiled function has {} element(s), "
                                                 "but the number of parameters in the function is {}",
-                                                pars->size(), m_impl->nparams));
+                                                pars->size(), m_impl->m_nparams));
     }
 
-    if (m_impl->is_time_dependent && !time) [[unlikely]] {
+    if (m_impl->m_is_time_dependent && !time) [[unlikely]] {
         throw std::invalid_argument("A time value must be passed in order to evaluate a time-dependent function");
     }
 
 #if defined(HEYOKA_HAVE_REAL)
 
     if constexpr (std::same_as<T, mppp::real>) {
-        if (m_impl->check_prec) {
+        if (m_impl->m_check_prec) {
             const auto prec_checker = [&](const auto &x) {
-                if (x.get_prec() != m_impl->prec) [[unlikely]] {
+                if (x.get_prec() != m_impl->m_prec) [[unlikely]] {
                     throw std::invalid_argument(fmt::format(
                         "An mppp::real with an invalid precision of {} was detected in the arguments to the evaluation "
                         "of a compiled function - the expected precision value is {}",
-                        x.get_prec(), m_impl->prec));
+                        x.get_prec(), m_impl->m_prec));
                 }
             };
 
@@ -348,7 +353,7 @@ void cfunc<T>::operator()(out_1d outputs, in_1d inputs, std::optional<in_1d> par
 #endif
 
     // Invoke the compiled function.
-    m_impl->fptr_scal(outputs.data(), inputs.data(), pars ? pars->data() : nullptr, time ? &*time : nullptr);
+    m_impl->m_fptr_scal(outputs.data(), inputs.data(), pars ? pars->data() : nullptr, time ? &*time : nullptr);
 }
 
 // Explicit instantiations.
