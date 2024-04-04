5d49455c986fe4f391385901ec900cf12acd4de9
Francesco Biscani <bluescarni@gmail.com>
2024-04-04 18:01:40 +0200

    More WIP.

--- tools/gha_conda_coverage.sh
+++ tools/gha_conda_coverage.sh
@@ -10,22 +10,39 @@ set -e
 sudo apt-get install wget
 
 # Install conda+deps.
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O miniconda.sh
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-conda create -y -q -p $deps_dir c-compiler cxx-compiler cmake llvmdev tbb-devel tbb libboost-devel 'mppp=1.*' sleef xtensor xtensor-blas blas blas-devel fmt spdlog lcov
+mamba create -y -p $deps_dir c-compiler cxx-compiler ninja cmake \
+    llvmdev tbb-devel tbb libboost-devel 'mppp=1.*' sleef xtensor \
+    xtensor-blas blas blas-devel fmt spdlog lcov
 source activate $deps_dir
 
 # Create the build dir and cd into it.
 mkdir build
 cd build
 
-# GCC build.
-cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_BUILD_TUTORIALS=ON -DHEYOKA_WITH_MPPP=yes -DHEYOKA_WITH_SLEEF=yes -DCMAKE_CXX_FLAGS="--coverage" -DBoost_NO_BOOST_CMAKE=ON
-make -j2 VERBOSE=1
+# Clear the compilation flags set up by conda.
+unset CXXFLAGS
+unset CFLAGS
+
+# Configure.
+cmake ../ -G Ninja \
+    -DCMAKE_PREFIX_PATH=$deps_dir \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DHEYOKA_BUILD_TESTS=yes \
+    -DHEYOKA_BUILD_TUTORIALS=ON \
+    -DHEYOKA_WITH_MPPP=yes \
+    -DHEYOKA_WITH_SLEEF=yes \
+    -DCMAKE_CXX_FLAGS="--coverage" \
+    -DCMAKE_CXX_FLAGS_DEBUG="-g -Og" \
+    -DBoost_NO_BOOST_CMAKE=ON
+
+# Build.
+ninja -v -j2
+
+# Run the tests.
 ctest -V -j2
 
 # Create lcov report
--- tools/gha_conda_release_static.sh
+++ tools/gha_conda_release_static.sh
@@ -10,13 +10,13 @@ set -e
 sudo apt-get install wget
 
 # Install conda+deps.
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O miniconda.sh
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-conda create -y -q -p $deps_dir c-compiler cxx-compiler cmake llvmdev tbb-devel tbb libboost-devel 'mppp=1.*' sleef xtensor xtensor-blas blas blas-devel fmt spdlog zlib
+mamba create -y -p $deps_dir c-compiler cxx-compiler ninja cmake \
+    llvmdev tbb-devel tbb libboost-devel 'mppp=1.*' sleef xtensor xtensor-blas \
+    blas blas-devel fmt spdlog zlib
 source activate $deps_dir
 
 # Create the build dir and cd into it.
@@ -24,8 +24,22 @@ mkdir build
 cd build
 
 # GCC build.
-cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Release -DHEYOKA_BUILD_TESTS=yes -DHEYOKA_BUILD_TUTORIALS=ON -DHEYOKA_WITH_MPPP=yes -DHEYOKA_WITH_SLEEF=yes -DBoost_NO_BOOST_CMAKE=ON -DHEYOKA_ENABLE_IPO=yes -DHEYOKA_FORCE_STATIC_LLVM=yes -DHEYOKA_HIDE_LLVM_SYMBOLS=yes
-make -j2 VERBOSE=1
+cmake ../ -G Ninja \
+    -DCMAKE_PREFIX_PATH=$deps_dir \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DHEYOKA_BUILD_TESTS=yes \
+    -DHEYOKA_BUILD_TUTORIALS=ON \
+    -DHEYOKA_WITH_MPPP=yes \
+    -DHEYOKA_WITH_SLEEF=yes \
+    -DBoost_NO_BOOST_CMAKE=ON \
+    -DHEYOKA_ENABLE_IPO=yes \
+    -DHEYOKA_FORCE_STATIC_LLVM=yes \
+    -DHEYOKA_HIDE_LLVM_SYMBOLS=yes
+
+# Build.
+ninja -v -j2
+
+# Run the tests.
 ctest -V -j2
 
 set +e
