380ee2d5e73a26ff802056d8176ff849b344c25c
Francesco Biscani <bluescarni@gmail.com>
2024-05-24 08:30:25 +0200

    jb08 review.

--- include/heyoka/model/jb08_tn.hpp
+++ include/heyoka/model/jb08_tn.hpp
@@ -9,12 +9,15 @@
 #ifndef HEYOKA_MODEL_JB08_TN_HPP
 #define HEYOKA_MODEL_JB08_TN_HPP
 
-#include <array>
-#include <cstdint>
+#include <concepts>
+#include <ranges>
+#include <tuple>
 #include <utility>
 #include <vector>
 
 #include <heyoka/config.hpp>
+#include <heyoka/detail/igor.hpp>
+#include <heyoka/detail/type_traits.hpp>
 #include <heyoka/detail/visibility.hpp>
 #include <heyoka/expression.hpp>
 #include <heyoka/kw.hpp>
@@ -28,131 +31,69 @@ namespace model
 namespace detail
 {
 
-// Common options for the nrlmsise00_tn functions.
+// Macro to reduce typing when handling kwargs.
+#define HEYOKA_MODEL_JB08_KWARG(name)                                                                                  \
+    auto name = [&p]() {                                                                                               \
+        if constexpr (p.has(kw::name) && std::constructible_from<expression, decltype(p(kw::name))>) {                 \
+            return expression{p(kw::name)};                                                                            \
+        } else {                                                                                                       \
+            static_assert(heyoka::detail::always_false_v<KwArgs...>,                                                   \
+                          "The '" #name "' keyword argument is necessary but either it was not provided, or it is of " \
+                          "the wrong type");                                                                           \
+        }                                                                                                              \
+    }()
+
+// Common options for the jb08_tn functions.
 template <typename... KwArgs>
 auto jb08_tn_common_opts(const KwArgs &...kw_args)
 {
     igor::parser p{kw_args...};
 
+    static_assert(!p.has_unnamed_arguments(), "This function accepts only named arguments");
+
     // Geodetic coordinates. Mandatory.
     std::vector<expression> geodetic;
+    geodetic.reserve(3);
     if constexpr (p.has(kw::geodetic)) {
-        for (const auto &val : p(kw::geodetic)) {
-            geodetic.emplace_back(val);
+        using geodetic_t = decltype(p(kw::geodetic));
+
+        if constexpr (requires() {
+                          requires std::ranges::input_range<geodetic_t>;
+                          requires std::constructible_from<expression, std::ranges::range_reference_t<geodetic_t>>;
+                      }) {
+            for (auto &&v : p(kw::geodetic)) {
+                geodetic.emplace_back(std::forward<decltype(v)>(v));
+            }
+        } else {
+            static_assert(
+                heyoka::detail::always_false_v<KwArgs...>,
+                "The 'geodetic' keyword argument is of the wrong type (it must be an input range whose reference "
+                "type can be used to construct an expression)");
         }
     } else {
         static_assert(heyoka::detail::always_false_v<KwArgs...>,
                       "The 'geodetic' keyword argument is necessary but it was not provided");
     }
 
-    // f107a index. Mandatory.
-    auto f107a = [&p]() -> expression {
-        if constexpr (p.has(kw::f107a)) {
-            return p(kw::f107a);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 'f107a' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // f107 index. Mandatory.
-    auto f107 = [&p]() -> expression {
-        if constexpr (p.has(kw::f107)) {
-            return p(kw::f107);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 'f107' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // s107a index. Mandatory.
-    auto s107a = [&p]() -> expression {
-        if constexpr (p.has(kw::s107a)) {
-            return p(kw::s107a);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 's107a' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // s107 index. Mandatory.
-    auto s107 = [&p]() -> expression {
-        if constexpr (p.has(kw::s107)) {
-            return p(kw::s107);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 's107' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // m107a index. Mandatory.
-    auto m107a = [&p]() -> expression {
-        if constexpr (p.has(kw::m107a)) {
-            return p(kw::m107a);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 'm107a' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // m107 index. Mandatory.
-    auto m107 = [&p]() -> expression {
-        if constexpr (p.has(kw::m107)) {
-            return p(kw::m107);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 'm107' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // y107a index. Mandatory.
-    auto y107a = [&p]() -> expression {
-        if constexpr (p.has(kw::y107a)) {
-            return p(kw::y107a);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 'y107a' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // y107 index. Mandatory.
-    auto y107 = [&p]() -> expression {
-        if constexpr (p.has(kw::y107)) {
-            return p(kw::y107);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 'y107' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // dDstdT index. Mandatory.
-    auto dDstdT = [&p]() -> expression {
-        if constexpr (p.has(kw::dDstdT)) {
-            return p(kw::dDstdT);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 'dDstdT' keyword argument is necessary but it was not provided");
-        }
-    }();
-
-    // Time expression. Mandatory. (In this case the fractional number of days elapsed since the last 1st of January
+    HEYOKA_MODEL_JB08_KWARG(f107a);
+    HEYOKA_MODEL_JB08_KWARG(f107);
+    HEYOKA_MODEL_JB08_KWARG(s107a);
+    HEYOKA_MODEL_JB08_KWARG(s107);
+    HEYOKA_MODEL_JB08_KWARG(m107a);
+    HEYOKA_MODEL_JB08_KWARG(m107);
+    HEYOKA_MODEL_JB08_KWARG(y107a);
+    HEYOKA_MODEL_JB08_KWARG(y107);
+    HEYOKA_MODEL_JB08_KWARG(dDstdT);
+    // NOTE: the time in this case is the fractional number of days elapsed since the last 1st of January
     // 00:00:00 UTC)
-    auto doy_expr = [&p]() -> expression {
-        if constexpr (p.has(kw::time)) {
-            return p(kw::time);
-        } else {
-            static_assert(heyoka::detail::always_false_v<KwArgs...>,
-                          "The 'time' keyword argument is necessary but it was not provided. Note that for thermonets "
-                          "this is expected to be an "
-                          "expression of the heyoka::time returning the number of days elapsed since the last 1st of "
-                          "January 00:00:00.");
-        }
-    }();
+    HEYOKA_MODEL_JB08_KWARG(time);
 
-    return std::tuple{std::move(geodetic), std::move(f107a),  std::move(f107),    std::move(s107a),
-                      std::move(s107),     std::move(m107a),  std::move(m107),    std::move(y107a),
-                      std::move(y107),     std::move(dDstdT), std::move(doy_expr)};
+    return std::tuple{std::move(geodetic), std::move(f107a),  std::move(f107), std::move(s107a),
+                      std::move(s107),     std::move(m107a),  std::move(m107), std::move(y107a),
+                      std::move(y107),     std::move(dDstdT), std::move(time)};
 }
+
+#undef HEYOKA_MODEL_JB08_KWARG
 
 // This c++ function returns the symbolic expressions of the thermospheric density at a certain geodetic coordinate,
 // having the f107a, f107, ap indexes and from a time expression returning the days elapsed since the last 1st of
