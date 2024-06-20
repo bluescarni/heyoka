92c57d16eef1315427fd36254c92f8bc36c3618a
Francesco Biscani <bluescarni@gmail.com>
2024-06-20 09:00:12 +0200

    Reduce repetition.

--- src/math/logical.cpp
+++ src/math/logical.cpp
@@ -44,7 +44,11 @@ std::vector<expression> logical_and_impl::gradient() const
 namespace
 {
 
-llvm::Value *logical_and_eval_impl(llvm_state &s, const std::vector<llvm::Value *> &args)
+// NOTE: perhaps it could be worth it to implement this via a pairwise reduction. We cannot
+// do this right now because we need to transform args via llvm_fnz() before performing the
+// reduction and pairwise_reduce() does not support this pre-transform.
+template <bool IsAnd>
+llvm::Value *logical_andor_eval_impl(llvm_state &s, const std::vector<llvm::Value *> &args)
 {
     assert(!args.empty());
 
@@ -54,7 +58,11 @@ llvm::Value *logical_and_eval_impl(llvm_state &s, const std::vector<llvm::Value 
 
     for (decltype(args.size()) i = 1; i < args.size(); ++i) {
         auto *tmp = llvm_fnz(s, args[i]);
-        ret = builder.CreateLogicalAnd(ret, tmp);
+        if constexpr (IsAnd) {
+            ret = builder.CreateLogicalAnd(ret, tmp);
+        } else {
+            ret = builder.CreateLogicalOr(ret, tmp);
+        }
     }
 
     return llvm_ui_to_fp(s, ret, args[0]->getType());
@@ -67,16 +75,17 @@ llvm::Value *logical_and_impl::llvm_eval(llvm_state &s, llvm::Type *fp_t, const 
                                          std::uint32_t batch_size, bool high_accuracy) const
 {
     return llvm_eval_helper(
-        [&s](const std::vector<llvm::Value *> &args, bool) { return logical_and_eval_impl(s, args); }, *this, s, fp_t,
-        eval_arr, par_ptr, stride, batch_size, high_accuracy);
+        [&s](const std::vector<llvm::Value *> &args, bool) { return logical_andor_eval_impl<true>(s, args); }, *this, s,
+        fp_t, eval_arr, par_ptr, stride, batch_size, high_accuracy);
 }
 
 llvm::Function *logical_and_impl::llvm_c_eval_func(llvm_state &s, llvm::Type *fp_t, std::uint32_t batch_size,
                                                    bool high_accuracy) const
 {
     return llvm_c_eval_func_helper(
-        "logical_and", [&s](const std::vector<llvm::Value *> &args, bool) { return logical_and_eval_impl(s, args); },
-        *this, s, fp_t, batch_size, high_accuracy);
+        "logical_and",
+        [&s](const std::vector<llvm::Value *> &args, bool) { return logical_andor_eval_impl<true>(s, args); }, *this, s,
+        fp_t, batch_size, high_accuracy);
 }
 
 logical_or_impl::logical_or_impl() : logical_or_impl({1_dbl}) {}
@@ -91,42 +100,22 @@ std::vector<expression> logical_or_impl::gradient() const
     return std::vector<expression>(this->args().size(), 0_dbl);
 }
 
-namespace
-{
-
-llvm::Value *logical_or_eval_impl(llvm_state &s, const std::vector<llvm::Value *> &args)
-{
-    assert(!args.empty());
-
-    auto &builder = s.builder();
-
-    auto *ret = llvm_fnz(s, args[0]);
-
-    for (decltype(args.size()) i = 1; i < args.size(); ++i) {
-        auto *tmp = llvm_fnz(s, args[i]);
-        ret = builder.CreateLogicalOr(ret, tmp);
-    }
-
-    return llvm_ui_to_fp(s, ret, args[0]->getType());
-}
-
-} // namespace
-
 llvm::Value *logical_or_impl::llvm_eval(llvm_state &s, llvm::Type *fp_t, const std::vector<llvm::Value *> &eval_arr,
                                         llvm::Value *par_ptr, llvm::Value *, llvm::Value *stride,
                                         std::uint32_t batch_size, bool high_accuracy) const
 {
     return llvm_eval_helper(
-        [&s](const std::vector<llvm::Value *> &args, bool) { return logical_or_eval_impl(s, args); }, *this, s, fp_t,
-        eval_arr, par_ptr, stride, batch_size, high_accuracy);
+        [&s](const std::vector<llvm::Value *> &args, bool) { return logical_andor_eval_impl<false>(s, args); }, *this,
+        s, fp_t, eval_arr, par_ptr, stride, batch_size, high_accuracy);
 }
 
 llvm::Function *logical_or_impl::llvm_c_eval_func(llvm_state &s, llvm::Type *fp_t, std::uint32_t batch_size,
                                                   bool high_accuracy) const
 {
     return llvm_c_eval_func_helper(
-        "logical_or", [&s](const std::vector<llvm::Value *> &args, bool) { return logical_or_eval_impl(s, args); },
-        *this, s, fp_t, batch_size, high_accuracy);
+        "logical_or",
+        [&s](const std::vector<llvm::Value *> &args, bool) { return logical_andor_eval_impl<false>(s, args); }, *this,
+        s, fp_t, batch_size, high_accuracy);
 }
 
 } // namespace detail
