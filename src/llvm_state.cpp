c0de5b5dbf84c72c117eebc9aff2a8ffd80a2bf6
Francesco Biscani <bluescarni@gmail.com>
2024-08-03 16:01:03 +0200

    Avoid another redundant copy.

--- src/llvm_state.cpp
+++ src/llvm_state.cpp
@@ -508,6 +508,37 @@ std::string ir_from_module(llvm::Module &m)
     return std::move(ostr.str());
 }
 
+// An implementation of llvm::MemoryBuffer offering a view over a std::string.
+class string_view_mem_buffer final : public llvm::MemoryBuffer
+{
+public:
+    explicit string_view_mem_buffer(const std::string &s)
+    {
+        // NOTE: the important bit here is from the LLVM docs:
+        //
+        // """
+        // In addition to basic access to the characters in the file, this interface
+        // guarantees you can read one character past the end of the file, and that
+        // this character will read as '\0'.
+        // """
+        //
+        // This is exactly the guarantee given by std::string:
+        //
+        // https://en.cppreference.com/w/cpp/string/basic_string/data
+        //
+        // Not sure about the third parameter to this function though, it does not
+        // seem to have any influence apart from debug checking:
+        //
+        // https://llvm.org/doxygen/MemoryBuffer_8cpp_source.html
+        this->init(s.data(), s.data() + s.size(), true);
+    }
+    llvm::MemoryBuffer::BufferKind getBufferKind() const final
+    {
+        // Hopefully std::string is not memory-mapped...
+        return llvm::MemoryBuffer::BufferKind::MemoryBuffer_Malloc;
+    }
+};
+
 // Helper to add an object file to the jit, throwing in case of errors.
 void add_obj_to_lljit(llvm::orc::LLJIT &lljit, const std::string &obj)
 {
@@ -518,37 +549,6 @@ void add_obj_to_lljit(llvm::orc::LLJIT &lljit, const std::string &obj)
     if (obj.empty()) {
         return;
     }
-
-    // An implementation of llvm::MemoryBuffer offering a view over a std::string.
-    class string_view_mem_buffer final : public llvm::MemoryBuffer
-    {
-    public:
-        explicit string_view_mem_buffer(const std::string &s)
-        {
-            // NOTE: the important bit here is from the LLVM docs:
-            //
-            // """
-            // In addition to basic access to the characters in the file, this interface
-            // guarantees you can read one character past the end of the file, and that
-            // this character will read as '\0'.
-            // """
-            //
-            // This is exactly the guarantee given by std::string:
-            //
-            // https://en.cppreference.com/w/cpp/string/basic_string/data
-            //
-            // Not sure about the third parameter to this function though, it does not
-            // seem to have any influence apart from debug checking:
-            //
-            // https://llvm.org/doxygen/MemoryBuffer_8cpp_source.html
-            this->init(s.data(), s.data() + s.size(), true);
-        }
-        llvm::MemoryBuffer::BufferKind getBufferKind() const final
-        {
-            // Hopefully std::string is not memory-mapped...
-            return llvm::MemoryBuffer::BufferKind::MemoryBuffer_Malloc;
-        }
-    };
 
     // Add the object file.
     auto err = lljit.addObjectFile(std::make_unique<string_view_mem_buffer>(obj));
@@ -818,11 +818,10 @@ void llvm_state_add_obj_to_jit(Jit &j, std::string obj)
 // contained in the bitcode) because apparently llvm::parseBitcodeFile() discards the module
 // name when parsing.
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-auto llvm_state_bc_to_module(const std::string &module_name, const std::string &bc, llvm::LLVMContext &ctx)
+auto bc_to_module(const std::string &module_name, const std::string &bc, llvm::LLVMContext &ctx)
 {
-    // Create the corresponding memory buffer.
-    auto mb = llvm::MemoryBuffer::getMemBuffer(bc);
-    assert(mb);
+    // Create the corresponding memory buffer view on bc.
+    auto mb = std::make_unique<string_view_mem_buffer>(bc);
 
     // Parse the bitcode.
     auto ret = llvm::parseBitcodeFile(mb->getMemBufferRef(), ctx);
@@ -931,7 +930,7 @@ llvm_state::llvm_state(const llvm_state &other)
         // module and builder. The IR/bitcode snapshots
         // are left in their default-constructed (empty)
         // state.
-        m_module = detail::llvm_state_bc_to_module(m_module_name, other.get_bc(), context());
+        m_module = detail::bc_to_module(m_module_name, other.get_bc(), context());
 
         // Create a new builder for the module.
         m_builder = std::make_unique<ir_builder>(context());
@@ -1127,7 +1126,7 @@ void llvm_state::load_impl(Archive &ar, unsigned version)
             m_bc_snapshot.clear();
 
             // Create the module from the bitcode.
-            m_module = detail::llvm_state_bc_to_module(m_module_name, bc_snapshot, context());
+            m_module = detail::bc_to_module(m_module_name, bc_snapshot, context());
 
             // Create a new builder for the module.
             m_builder = std::make_unique<ir_builder>(context());
