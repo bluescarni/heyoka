0483e4894be7eda91a8b3c881c053fd32b74848a
Francesco Biscani <bluescarni@gmail.com>
2025-04-04 22:27:50 +0200

    Initial addition of the datafiles and the converter.

---
+++ include/heyoka/detail/egm2008.hpp
@@ -0,0 +1,25 @@
+// Copyright 2020-2025 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+//
+// This file is part of the heyoka library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef HEYOKA_DETAIL_EGM2008_HPP
+#define HEYOKA_DETAIL_EGM2008_HPP
+
+#include <heyoka/config.hpp>
+
+HEYOKA_BEGIN_NAMESPACE
+
+namespace model::detail
+{
+
+extern const double egm2008_CS[3318][2];
+
+} // namespace model::detail
+
+HEYOKA_END_NAMESPACE
+
+#endif
