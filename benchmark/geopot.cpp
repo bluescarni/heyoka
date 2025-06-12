b3e889b1f6847e6eef79955d6b265af62f83499a
Francesco Biscani <bluescarni@gmail.com>
2025-06-12 11:51:18 +0200

    Work on the geopot benchmark.

--- benchmark/geopot.cpp
+++ benchmark/geopot.cpp
@@ -7,11 +7,18 @@
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <stdexcept>
 #include <vector>
+
+#include <boost/program_options.hpp>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/stopwatch.h>
 
+#include <heyoka/ensemble_propagate.hpp>
 #include <heyoka/expression.hpp>
 #include <heyoka/kw.hpp>
 #include <heyoka/logging.hpp>
@@ -20,21 +27,48 @@
 
 using namespace heyoka;
 
-int main(int, char *[])
+int main(int argc, char *argv[])
 {
+    namespace po = boost::program_options;
+
+    // Max degree/order for the geopotential.
+    std::uint32_t max_degree = 0;
+    // Integration tolerance.
+    auto tol = 0.;
+    // Number of iterations to be run in parallel.
+    std::size_t npariter = 0u;
+
+    po::options_description desc("Options");
+
+    desc.add_options()("help", "produce help message")("tol", po::value<double>(&tol)->default_value(1e-15),
+                                                       "tolerance")(
+        "max_degree", po::value<std::uint32_t>(&max_degree)->default_value(10u),
+        "maximum degree/order for the geopotential")("npariter", po::value<std::size_t>(&npariter)->default_value(1u),
+                                                     "number of iterations to be run in parallel");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.contains("help")) {
+        std::cout << desc << "\n";
+        return 0;
+    }
+
+    if (npariter == 0u) {
+        throw std::invalid_argument("The number of parallel iterations cannot be zero");
+    }
+
     // Fetch the logger.
     create_logger();
     set_logger_level_trace();
     auto logger = spdlog::get("heyoka");
 
-    // Maximum degree/order for the geopotential.
-    const auto n = 10u, m = 10u;
-
     // State variables.
     const auto [x, y, z, vx, vy, vz] = make_vars("x", "y", "z", "vx", "vy", "vz");
 
     // Acceleration vector.
-    const auto [acc_x, acc_y, acc_z] = model::egm2008_acc({x, y, z}, n, m);
+    const auto [acc_x, acc_y, acc_z] = model::egm2008_acc({x, y, z}, max_degree, max_degree);
 
     // Initial conditions in LEO.
     const auto ic_leo = std::vector{6740440.0, 0.0, 0.0, 0.0, 6725.973853066024, 3883.2537950295855};
@@ -50,7 +84,15 @@ int main(int, char *[])
         spdlog::stopwatch sw;
         ta.set_time(0.);
         std::ranges::copy(ic_leo, ta.get_state_data());
-        ta.propagate_until(86400.);
+
+        if (npariter == 1u) {
+            ta.propagate_until(86400.);
+        } else {
+            const auto gen = std::function<taylor_adaptive<double>(taylor_adaptive<double>, std::size_t)>(
+                [](auto ta, auto) { return ta; });
+            ensemble_propagate_until(ta, 86400., npariter, gen);
+        }
+
         logger->trace("Total runtime: {}s", sw);
     }
 }
