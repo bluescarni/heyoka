6ee5b5b14ed32cbd96cd0851e503c53b8d5acf6b
Francesco Biscani <bluescarni@gmail.com>
2026-01-08 14:02:13 +0100

    Attempt to fix the CI build.

--- tools/gha_osx_arm64_static.sh
+++ tools/gha_osx_arm64_static.sh
@@ -27,7 +27,7 @@ unset CFLAGS
 # Configure.
 #
 # NOTE: turn off tutorials until the fmt+malloc compilation failure is resolved.
-CXX=clang++ CC=clang cmake -G Ninja ../ \
+CXX=$CONDA_PREFIX/clang++ CC=$CONDA_PREFIX/clang cmake -G Ninja ../ \
     -DCMAKE_PREFIX_PATH=$deps_dir \
     -DCMAKE_BUILD_TYPE=Debug \
     -DHEYOKA_BUILD_TESTS=yes \
