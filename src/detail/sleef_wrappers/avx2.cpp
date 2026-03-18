b63f64e6c40e09a193a435339650014a2b3a2ace
Francesco Biscani <bluescarni@gmail.com>
2026-03-18 13:56:22 +0100

    Tentative MSVC workarounds.

--- src/detail/sleef_wrappers/avx2.cpp
+++ src/detail/sleef_wrappers/avx2.cpp
@@ -6,6 +6,12 @@
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+// NOTE: MSVC does not define __SSE2__ even though SSE2 is the baseline on x64. SLEEF's header guards its AVX2-128
+// declarations behind #ifdef __SSE2__, so we need to define it manually.
+#if defined(_MSC_VER) && !defined(__SSE2__)
+#define __SSE2__
+#endif
+
 #include "sleef_wrappers_helpers.hpp"
 
 // Double-precision.
