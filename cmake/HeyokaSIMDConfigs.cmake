c3abdf2892dfd6a8f39154db1073b0ca7c75dbba
Francesco Biscani <bluescarni@gmail.com>
2026-03-18 11:30:24 +0100

    Fix HeyokaSIMDConfigs to avoid caching issues when checking for compiler flags.

--- cmake/HeyokaSIMDConfigs.cmake
+++ cmake/HeyokaSIMDConfigs.cmake
@@ -1,5 +1,25 @@
 include(CheckCXXCompilerFlag)
 
+# Helper macro: check a list of compiler flag candidates for a given ISA level.
+#
+# The first candidate that succeeds is used. Each candidate gets a unique cache variable
+# (via string(MAKE_C_IDENTIFIER)) to avoid the CMake cache trap where a failed check for
+# one candidate (e.g., -msse2 on MSVC) would prevent subsequent candidates (e.g., /arch:SSE2)
+# from being tested.
+macro(_heyoka_check_simd_flag _isa _suffixes _flags)
+    set(_${_isa}_found FALSE)
+    foreach(_candidate ${ARGN})
+        string(MAKE_C_IDENTIFIER "${_candidate}" _candidate_id)
+        check_cxx_compiler_flag("${_candidate}" _HEYOKA_HAS_${_isa}_${_candidate_id})
+        if(_HEYOKA_HAS_${_isa}_${_candidate_id})
+            list(APPEND ${_suffixes} "${_isa}")
+            list(APPEND ${_flags} "${_candidate}")
+            set(_${_isa}_found TRUE)
+            break()
+        endif()
+    endforeach()
+endmacro()
+
 function(heyoka_get_simd_configs OUT_SUFFIXES OUT_FLAGS)
     set(_suffixes "")
     set(_flags "")
@@ -8,54 +28,19 @@ function(heyoka_get_simd_configs OUT_SUFFIXES OUT_FLAGS)
 
     # x86.
     if(_proc MATCHES "x86|amd64")
-        # SSE2.
-        foreach(_candidate "-msse2" "/arch:SSE2")
-            check_cxx_compiler_flag("${_candidate}" _HEYOKA_HAS_SSE2)
-            if(_HEYOKA_HAS_SSE2)
-                list(APPEND _suffixes "sse2")
-                list(APPEND _flags "${_candidate}")
-                break()
-            endif()
-        endforeach()
-
-        # AVX.
-        foreach(_candidate "-mavx" "/arch:AVX")
-            check_cxx_compiler_flag("${_candidate}" _HEYOKA_HAS_AVX)
-            if(_HEYOKA_HAS_AVX)
-                list(APPEND _suffixes "avx")
-                list(APPEND _flags "${_candidate}")
-                break()
-            endif()
-        endforeach()
-
-        # AVX2.
-        foreach(_candidate "-mavx2" "/arch:AVX2")
-            check_cxx_compiler_flag("${_candidate}" _HEYOKA_HAS_AVX2)
-            if(_HEYOKA_HAS_AVX2)
-                list(APPEND _suffixes "avx2")
-                list(APPEND _flags "${_candidate}")
-                break()
-            endif()
-        endforeach()
-
-        # AVX-512.
-        foreach(_candidate "-mavx512f" "/arch:AVX512")
-            check_cxx_compiler_flag("${_candidate}" _HEYOKA_HAS_AVX512)
-            if(_HEYOKA_HAS_AVX512)
-                list(APPEND _suffixes "avx512")
-                list(APPEND _flags "${_candidate}")
-                break()
-            endif()
-        endforeach()
+        _heyoka_check_simd_flag(sse2 _suffixes _flags "-msse2" "/arch:SSE2")
+        _heyoka_check_simd_flag(avx _suffixes _flags "-mavx" "/arch:AVX")
+        _heyoka_check_simd_flag(avx2 _suffixes _flags "-mavx2" "/arch:AVX2")
+        _heyoka_check_simd_flag(avx512 _suffixes _flags "-mavx512f" "/arch:AVX512")
 
         # Sanity checks: higher ISA levels imply lower ones.
-        if(_HEYOKA_HAS_AVX AND NOT _HEYOKA_HAS_SSE2)
+        if(_avx_found AND NOT _sse2_found)
             message(FATAL_ERROR "AVX detected but SSE2 is not available - this should not happen.")
         endif()
-        if(_HEYOKA_HAS_AVX2 AND NOT _HEYOKA_HAS_AVX)
+        if(_avx2_found AND NOT _avx_found)
             message(FATAL_ERROR "AVX2 detected but AVX is not available - this should not happen.")
         endif()
-        if(_HEYOKA_HAS_AVX512 AND NOT _HEYOKA_HAS_AVX2)
+        if(_avx512_found AND NOT _avx2_found)
             message(FATAL_ERROR "AVX-512 detected but AVX2 is not available - this should not happen.")
         endif()
     # AArch64: AdvSIMD is always available as baseline.
