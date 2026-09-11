a690cdce50609e1c5971060f9a29017d465167f7
Francesco Biscani <bluescarni@gmail.com>
2026-09-11 22:08:54 +0200

    Test fix.

--- test/llvm_multi_state.cpp
+++ test/llvm_multi_state.cpp
@@ -204,10 +204,8 @@ TEST_CASE("copy semantics")
         auto *cf2_ptr = reinterpret_cast<void (*)(double *, const double *, const double *, const double *)>(
             ms_copy.jit_lookup("f2"));
 
-        REQUIRE_THROWS_MATCHES(
-            ms_copy.jit_lookup("f3"), std::invalid_argument,
-            Message("Could not find the symbol 'f3' in an llvm_multi_state. The full error message:\n"
-                    "Symbols not found: [ f3 ]"));
+        REQUIRE_THROWS_WITH(ms_copy.jit_lookup("f3"),
+                            Catch::Matchers::StartsWith("Could not find the symbol 'f3' in an llvm_multi_state."));
 
         const double ins[] = {2., 3.};
         double outs[2] = {};
