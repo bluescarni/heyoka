6bf2790b77638cb82724527a0389f7566e1dd279
Francesco Biscani <bluescarni@gmail.com>
2026-09-22 15:00:38 +0200

    Update builtin eop/sw data.

--- include/heyoka/detail/eop_data/builtin_eop_data.hpp
+++ include/heyoka/detail/eop_data/builtin_eop_data.hpp
@@ -19,7 +19,7 @@ namespace detail
 
 extern const char *const builtin_eop_data_ts;
 
-extern const eop_data_row builtin_eop_data[19628];
+extern const eop_data_row builtin_eop_data[19684];
 
 } // namespace detail
 
